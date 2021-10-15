5630567252778e200a941d2a7f28393cdd1f3cbe
bairon-tapia <b.tapia.morales@gmail.com>
2021-10-15 18:27:52 -0300

    Initial commit

---
+++ src/main/java/com/bairontapia/projects/cuidamed/CuidaMedApplication.java
@@ -0,0 +1,13 @@
+package com.bairontapia.projects.cuidamed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuidaMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuidaMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bairontapia/projects/cuidamed/CuidaMedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bairontapia.projects.cuidamed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidaMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
