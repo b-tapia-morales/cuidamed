8f5f6730be6dc332b375d688a213b852d7faa31a
bairon-tapia <b.tapia.morales@gmail.com>
2021-10-15 20:37:15 -0300

    Create tables on schema

---
+++ src/main/resources/scripts/schema.sql
@@ -0,0 +1,30 @@
+
+CREATE DATABASE
+
+CREATE TABLE region IF NOT EXISTS
+(
+    id SMALLSERIAL PRIMARY KEY,
+    region_name VARCHAR(64) UNIQUE NOT NULL,
+    abbreviation VARCHAR(4) UNIQUE NOT NULL,
+    capital VARCHAR(64) UNIQUE NOT NULL,
+);
+
+INSERT INTO region (region_name, abbreviation, capital)
+VALUES ('Arica y Parinacota', 'AP', 'Arica'),
+       ('Tarapacá', 'TA', 'Iquique'),
+       ('Antofagasta', 'AN', 'Antofagasta'),
+       ('Atacama', 'AT', 'Copiapó'),
+       ('Coquimbo', 'CO', 'La Serena'),
+       ('Valparaiso', 'VA', 'valparaíso'),
+       ('Metropolitana de Santiago', 'RM', 'Santiago'),
+       ('Libertador General Bernardo O''Higgins', 'OH', 'Rancagua'),
+       ('Maule', 'MA', 'Talca'),
+       ('Ñuble', 'NB', 'Chillán'),
+       ('Biobío', 'BI', 'Concepción'),
+       ('La Araucanía', 'IAR', 'Temuco'),
+       ('Los Ríos', 'LR', 'Valdivia'),
+       ('Los Lagos', 'LL', 'Puerto Montt'),
+       ('Aysén del General Carlos Ibáñez del Campo', 'AI', 'Coyhaique'),
+       ('Magallanes y de la Antártica Chilena', 'MG', 'Punta Arenas');
+
+
