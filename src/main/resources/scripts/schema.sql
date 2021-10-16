decc2742e85bb01a61ede98dc7349801b6b8b1e4
bairon-tapia <btapiam@alumnosuls.cl>
2021-10-16 14:25:15 -0300

    Added database creation script

---
+++ src/main/resources/scripts/schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE cuidamed;
+
+CREATE DATABASE cuidamed
+    WITH OWNER = postgres
+    ENCODING 'UTF8'
+    LC_COLLATE = 'es_ES.UTF-8'
+    LC_CTYPE = 'es_ES.UTF-8'
+    TEMPLATE template0;
