0d208aa8448be4ed9801eab451fd656706785682
Benjamín López <benjamin.lopezl@userena.cl>
2021-10-17 11:32:19 -0300

    allergies

--- src/main/resources/scripts/medical_record.sql
+++ src/main/resources/scripts/medical_record.sql
@@ -1,25 +1,28 @@
 CREATE TABLE IF NOT EXISTS residence.medical_record
 (
     elder_rut   VARCHAR(16) PRIMARY KEY REFERENCES residence.elder (rut),
-    blood_type  VARCHAR(4) NOT NULL,
-    healthcare_system   VARCHAR(20) NOT NULL
+    blood_type  SMALLINT NOT NULL,
+    healthcare_system  SMALLINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS residence.routine_checkup
 (
     elder_rut           VARCHAR(16) NOT NULL REFERENCES residence.elder (rut),
-    checkup_date        DATE    NOT NULL,
-    height              FLOAT   NOT NULL,
-    weight              FLOAT   NOT NULL,
-    BMI                 FLOAT   NOT NULL,
-    heart_rate          INT     NOT NULL,
-    diastolic_pressure  FLOAT   NOT NULL,
-    systolic_pressure   FLOAT   NOT NULL,
-    body_temperature    FLOAT   NOT NULL,
+    checkup_date        DATE        NOT NULL,
+    height              REAL       NOT NULL,
+    weight              REAL       NOT NULL,
+    BMI                 REAL       NOT NULL,
+    heart_rate          SMALLINT    NOT NULL,
+    diastolic_pressure  REAL       NOT NULL,
+    systolic_pressure   REAL       NOT NULL,
+    body_temperature    REAL       NOT NULL,
     PRIMARY KEY (elder_rut, checkup_date)
 );
 
 CREATE TABLE IF NOT EXISTS residence.allergy
 (
-
+    elder_rut           VARCHAR(16) NOT NULL REFERENCES residence.elder (rut),
+    allergy_name        VARCHAR(24) NOT NULL,
+    allergy_type        INT         NOT NULL,
+    PRIMARY KEY (elder_rut, allergy_name)
 );
