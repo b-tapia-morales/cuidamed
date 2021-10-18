5e4ce508645e5d7048a8e307fd9f14765126b8c9
bairon-tapia <btapiam@alumnosuls.cl>
2021-10-18 15:30:06 -0300

    Fixed issue #5

--- src/main/resources/scripts/prescription.sql
+++ src/main/resources/scripts/prescription.sql
@@ -6,42 +6,51 @@ DROP TABLE IF EXISTS residence.disease;
 
 CREATE TABLE IF NOT EXISTS residence.disease
 (
-    disease_name VARCHAR(32) PRIMARY KEY,
-    disease_type SMALLINT NOT NULL,
-    is_chronic   BOOLEAN
+    disease_name VARCHAR(32) NOT NULL,
+    disease_type SMALLINT    NOT NULL,
+    is_chronic   BOOLEAN     NOT NULL,
+    PRIMARY KEY (disease_name)
 );
 
 CREATE TABLE IF NOT EXISTS residence.elder_suffers_disease
 (
-    elder_rut      VARCHAR(16) REFERENCES residence.person (rut),
-    disease_name   VARCHAR(32) REFERENCES residence.disease (disease_name),
-    diagnosis_date DATE,
+    elder_rut      VARCHAR(16) NOT NULL,
+    disease_name   VARCHAR(32) NOT NULL,
+    diagnosis_date DATE        NOT NULL,
+    FOREIGN KEY (elder_rut) REFERENCES residence.elder (rut),
+    FOREIGN KEY (disease_name) REFERENCES residence.disease (disease_name),
     PRIMARY KEY (elder_rut, disease_name, diagnosis_date)
 );
 
 CREATE TABLE IF NOT EXISTS residence.medication
 (
-    medication_name      VARCHAR(64) PRIMARY KEY,
-    administration_route SMALLINT NOT NULL,
-    pharmaceutical_form  SMALLINT NOT NULL,
-    measure_unit         SMALLINT NOT NULL
+    medication_name      VARCHAR(64) NOT NULL,
+    administration_route SMALLINT    NOT NULL,
+    pharmaceutical_form  SMALLINT    NOT NULL,
+    measure_unit         SMALLINT    NOT NULL,
+    PRIMARY KEY (medication_name)
 );
 
 CREATE TABLE IF NOT EXISTS residence.prescription
 (
-    elder_rut         VARCHAR(16) REFERENCES residence.elder (rut),
-    disease_name      VARCHAR(32) REFERENCES residence.disease (disease_name),
-    prescription_date DATE,
-    description       TEXT NOT NULL,
+    elder_rut         VARCHAR(16) NOT NULL,
+    disease_name      VARCHAR(32) NOT NULL,
+    prescription_date DATE        NOT NULL,
+    description       TEXT        NOT NULL,
+    FOREIGN KEY (elder_rut) REFERENCES residence.elder (rut),
+    FOREIGN KEY (disease_name) REFERENCES residence.disease (disease_name),
     PRIMARY KEY (elder_rut, disease_name, prescription_date)
 );
 
 CREATE TABLE IF NOT EXISTS residence.medication_prescription
 (
-    elder_rut         VARCHAR(16) REFERENCES residence.prescription (elder_rut),
-    prescription_date DATE REFERENCES residence.prescription (prescription_date),
-    medication_name   VARCHAR(64) REFERENCES residence.medication (medication_name),
-    start_date        DATE NOT NULL,
+    elder_rut         VARCHAR(16) NOT NULL,
+    disease_name      VARCHAR(32) NOT NULL,
+    prescription_date DATE        NOT NULL,
+    medication_name   VARCHAR(64) NOT NULL,
+    start_date        DATE        NOT NULL,
     end_date          DATE,
-    PRIMARY KEY (elder_rut, prescription_date, medication_name)
+    FOREIGN KEY (elder_rut, disease_name, prescription_date) REFERENCES residence.prescription (elder_rut, disease_name, prescription_date),
+    FOREIGN KEY (medication_name) REFERENCES residence.medication (medication_name),
+    PRIMARY KEY (elder_rut, disease_name, prescription_date, medication_name)
 );
