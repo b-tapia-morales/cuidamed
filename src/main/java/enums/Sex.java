d0f6caff6a83ef94e44b24387f1917dd511d6c98
bairon-tapia <btapiam@alumnosuls.cl>
2021-10-18 20:09:23 -0300

    Changed variable names to fit enum field names

--- src/main/java/enums/Sex.java
+++ src/main/java/enums/Sex.java
@@ -16,13 +16,13 @@ public enum Sex {
     private final int code;
     private final String value;
 
-    private static final Sex[] VALUES = values();
-    private static final int[] CODES = Stream.of(VALUES).mapToInt(Sex::getCode).toArray();
-    private static final String[] REPRESENTATIONS = Stream.of(VALUES).map(Sex::getValue).toArray(String[]::new);
+    private static final Sex[] ARRAY = values();
+    private static final int[] CODES = Stream.of(ARRAY).mapToInt(Sex::getCode).toArray();
+    private static final String[] VALUES = Stream.of(ARRAY).map(Sex::getValue).toArray(String[]::new);
     private static final Map<Integer, String> MAP = IntStream
-            .range(0, VALUES.length)
+            .range(0, ARRAY.length)
             .boxed()
-            .collect(Collectors.toMap(i -> CODES[i], i -> REPRESENTATIONS[i], (a, b) -> a, LinkedHashMap::new));
+            .collect(Collectors.toMap(i -> CODES[i], i -> VALUES[i], (a, b) -> a, LinkedHashMap::new));
 
     Sex(final int code, final String value) {
         this.code = code;
