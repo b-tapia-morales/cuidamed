3758dd5511c135deb2aa5c32a1de337d048e6746
Benjamín López <benjamin.lopezl@userena.cl>
2021-10-19 14:34:09 -0300

    Changed previous format to google's standard.

--- src/main/java/com/bairontapia/projects/cuidamed/mappings/dosage_status/DosageStatus.java
+++ src/main/java/com/bairontapia/projects/cuidamed/mappings/dosage_status/DosageStatus.java
@@ -7,46 +7,46 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public enum DosageStatus {
-    UNDEFINED("Sin definir"),
-    PENDING("Pendiente"),
-    UNFULFILLED("Incumplido"),
-    FULFILLED("Cumplido");
+  UNDEFINED("Sin definir"),
+  PENDING("Pendiente"),
+  UNFULFILLED("Incumplido"),
+  FULFILLED("Cumplido");
 
-    private final String status;
-    private static final DosageStatus[] VALUES = values();
+  private final String status;
+  private static final DosageStatus[] VALUES = values();
 
-    DosageStatus(final String status) {
-        this.status = status;
+  DosageStatus(final String status) {
+    this.status = status;
+  }
+
+  @Override
+  public String toString() {
+    return status;
+  }
+
+  public String getStatus() {
+    return this.status;
+  }
+
+  public static DosageStatus[] getVALUES() {
+    return VALUES;
+  }
+
+  /*
+  VALUES[0] <-> 1
+  VALUES[1] <-> 2
+  VALUES[2] <-> 3
+  ...
+  VALUES[n] <-> n+1
+   */
+  public int getIndex() {
+    return ordinal() + 1;
+  }
+
+  public static DosageStatus getValueFromIndex(final int index) {
+    if (index < 1 | index > VALUES.length) {
+      throw new IllegalArgumentException();
     }
-
-    @Override
-    public String toString() {
-        return status;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
-
-    public static DosageStatus[] getVALUES() {
-        return VALUES;
-    }
-
-    /*
-    VALUES[0] <-> 1
-    VALUES[1] <-> 2
-    VALUES[2] <-> 3
-    ...
-    VALUES[n] <-> n+1
-     */
-    public int getIndex() {
-        return ordinal() + 1;
-    }
-
-    public static DosageStatus getValueFromIndex(final int index) {
-        if (index < 1 | index > VALUES.length) {
-            throw new IllegalArgumentException();
-        }
-        return VALUES[index - 1];
-    }
+    return VALUES[index - 1];
+  }
 }
--- src/main/java/com/bairontapia/projects/cuidamed/mappings/dosage_status/DosageStatusConverter.java
+++ src/main/java/com/bairontapia/projects/cuidamed/mappings/dosage_status/DosageStatusConverter.java
@@ -7,16 +7,15 @@ import java.util.Objects;
 @Converter
 public class DosageStatusConverter implements AttributeConverter<DosageStatus, Short> {
 
-    @Override
-    public Short convertToDatabaseColumn(DosageStatus value) {
-        Objects.requireNonNull(value);
-        return (short) value.getIndex();
-    }
+  @Override
+  public Short convertToDatabaseColumn(DosageStatus value) {
+    Objects.requireNonNull(value);
+    return (short) value.getIndex();
+  }
 
-    @Override
-    public DosageStatus convertToEntityAttribute(Short index) {
-        Objects.requireNonNull(index);
-        return DosageStatus.getValueFromIndex(index);
-    }
-
+  @Override
+  public DosageStatus convertToEntityAttribute(Short index) {
+    Objects.requireNonNull(index);
+    return DosageStatus.getValueFromIndex(index);
+  }
 }
