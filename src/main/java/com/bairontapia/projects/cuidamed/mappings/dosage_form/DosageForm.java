83578c3182e9f57b31d407128d78a72d1702b462
Benjamín López <benjamin.lopezl@userena.cl>
2021-10-19 16:37:17 -0300

    Added DosageForm enum and its converter class. DosageForms are based on their physical form

---
+++ src/main/java/com/bairontapia/projects/cuidamed/mappings/dosage_form/DosageForm.java
@@ -0,0 +1,38 @@
+package com.bairontapia.projects.cuidamed.mappings.dosage_form;
+
+import com.bairontapia.projects.cuidamed.mappings.dosage_status.DosageStatus;
+
+public enum DosageForm {
+  SOLID("Solidos"),
+  SEMI_SOLID("Semi-solidos"),
+  LIQUID("Liquidos"),
+  Gaseous("Gaseosos");
+
+  private static final DosageForm[] VALUES = values();
+
+  private final String form;
+
+  DosageForm(final String status) {
+    this.form = status;
+  }
+
+  public static DosageForm[] getValues() {
+    return VALUES;
+  }
+
+  public static DosageForm getValueFromIndex(final int index) {
+    if (index < 1 || index > VALUES.length) {
+      throw new IllegalArgumentException();
+    }
+    return VALUES[index - 1];
+  }
+
+  @Override
+  public String toString() {
+    return form;
+  }
+
+  public String getForm() {
+    return this.form;
+  }
+}
