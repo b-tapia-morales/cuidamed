112c90b884bbcd33c269c8304bd81d6115b337c2
bairon-tapia <btapiam@alumnosuls.cl>
2021-10-19 14:57:05 -0300

    Formatting issues

--- src/main/java/com/bairontapia/projects/cuidamed/mappings/dosage_status/DosageStatus.java
+++ src/main/java/com/bairontapia/projects/cuidamed/mappings/dosage_status/DosageStatus.java
@@ -1,22 +1,28 @@
 package com.bairontapia.projects.cuidamed.mappings.dosage_status;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 public enum DosageStatus {
   UNDEFINED("Sin definir"),
   PENDING("Pendiente"),
   UNFULFILLED("Incumplido"),
   FULFILLED("Cumplido");
 
-  private final String status;
   private static final DosageStatus[] VALUES = values();
+
+  private final String status;
 
   DosageStatus(final String status) {
     this.status = status;
+  }
+
+  public static DosageStatus[] getValues() {
+    return VALUES;
+  }
+
+  public static DosageStatus getValueFromIndex(final int index) {
+    if (index < 1 || index > VALUES.length) {
+      throw new IllegalArgumentException();
+    }
+    return VALUES[index - 1];
   }
 
   @Override
@@ -26,27 +32,5 @@ public enum DosageStatus {
 
   public String getStatus() {
     return this.status;
-  }
-
-  public static DosageStatus[] getVALUES() {
-    return VALUES;
-  }
-
-  /*
-  VALUES[0] <-> 1
-  VALUES[1] <-> 2
-  VALUES[2] <-> 3
-  ...
-  VALUES[n] <-> n+1
-   */
-  public int getIndex() {
-    return ordinal() + 1;
-  }
-
-  public static DosageStatus getValueFromIndex(final int index) {
-    if (index < 1 | index > VALUES.length) {
-      throw new IllegalArgumentException();
-    }
-    return VALUES[index - 1];
   }
 }
