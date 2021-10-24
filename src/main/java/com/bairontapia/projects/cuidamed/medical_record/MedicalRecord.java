482f69911e5b48faa056aa938e502c91f60aa852
Benjamín López <benjamin.lopezl@userena.cl>
2021-10-24 18:03:28 -0300

    created MedicalRecord class including its fields.

---
+++ src/main/java/com/bairontapia/projects/cuidamed/medical_record/MedicalRecord.java
@@ -0,0 +1,28 @@
+package com.bairontapia.projects.cuidamed.medical_record;
+
+import com.bairontapia.projects.cuidamed.mappings.bloodtype.BloodType;
+import com.bairontapia.projects.cuidamed.mappings.bloodtype.BloodTypeConverter;
+import com.bairontapia.projects.cuidamed.mappings.healthsystem.HealthSystem;
+import com.bairontapia.projects.cuidamed.mappings.healthsystem.HealthSystemConverter;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(schema = "residence", name = "medical_record")
+@Getter
+@Setter
+public class MedicalRecord {
+    @Id
+    @Column(name = "elder_rut",unique = true, nullable = false, updatable = false)
+    private String elderRut;
+
+    @Column(name = "blood_type", nullable = false)
+    @Convert(converter = BloodTypeConverter.class)
+    private BloodType bloodType;
+
+    @Column(name = "healthcare_system", nullable = false)
+    @Convert(converter = HealthSystemConverter.class)
+    private HealthSystem healthcareSystem;
+}
