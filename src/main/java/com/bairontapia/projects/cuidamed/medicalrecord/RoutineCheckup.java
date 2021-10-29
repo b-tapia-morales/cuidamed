07ac8e03a9c29245380c5ecafa6435db50d8ce84
bairon-tapia <btapiam@alumnosuls.cl>
2021-10-28 21:52:29 -0300

    Formatting issues

--- src/main/java/com/bairontapia/projects/cuidamed/medicalrecord/RoutineCheckup.java
+++ src/main/java/com/bairontapia/projects/cuidamed/medicalrecord/RoutineCheckup.java
@@ -1,6 +1,5 @@
 package com.bairontapia.projects.cuidamed.medicalrecord;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,28 +14,26 @@ import java.io.Serializable;
 @Getter
 @Setter
 public class RoutineCheckup implements Serializable {
-    @EmbeddedId
-    private RoutineCheckupPK routineCheckupPK;
+  @EmbeddedId private RoutineCheckupId routineCheckupId;
 
-    @Column(name = "height", nullable = false)
-    private Double height;
+  @Column(name = "height", nullable = false)
+  private Double height;
 
-    @Column(name = "weight", nullable = false)
-    private Double weight;
+  @Column(name = "weight", nullable = false)
+  private Double weight;
 
-    @Column(name = "bmi", nullable = false)
-    private Double bmi;
+  @Column(name = "bmi", nullable = false)
+  private Double bmi;
 
-    @Column(name = "heart_rate", nullable = false)
-    private Integer heartRate;
+  @Column(name = "heart_rate", nullable = false)
+  private Integer heartRate;
 
-    @Column(name = "diastolic_pressure", nullable = false)
-    private Double diastolicPressure;
+  @Column(name = "diastolic_pressure", nullable = false)
+  private Double diastolicPressure;
 
-    @Column(name = "systolic_preassure", nullable = false)
-    private Double systolicPreassure;
+  @Column(name = "systolic_pressure", nullable = false)
+  private Double systolicPressure;
 
-    @Column(name = "body_temperature", nullable = false)
-    private Double bodyTemperature;
-
+  @Column(name = "body_temperature", nullable = false)
+  private Double bodyTemperature;
 }
