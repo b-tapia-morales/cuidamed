0082bfd11966d3a1a05277f06d598c2a61fa40fc
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-04 08:21:28 -0300

    Revert "Merge branch 'master' of https://github.com/b-tapia-morales/cuidamed"
    This reverts commit 6748ba395f58fd3fb5d7e57ae84e89544c7aed6b, reversing
    changes made to 3ed93ae438a1f2e739d67706f1a81594f8ac700d.

--- src/main/resources/scripts/data_people.sql
+++ src/main/resources/scripts/data_people.sql
@@ -31,36 +31,6 @@ ON CONFLICT DO NOTHING;
 INSERT INTO residence.person
 VALUES ('5902831-6', 'Luis Alberto', 'Contreras', 'Parra', '1951-11-30', 1)
 ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('93566783-6', 'José Antonio', 'Pérez', 'Pérez', '1921-01-12', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('33354753-8', 'Alberto Eduardo', 'Campos', 'Sastre', '1937-02-22', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('86838888-9', 'Elvie Jito', 'Rojas', 'Álvarez', '1900-01-01', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('93122719-K', 'Sebastian Enrique', 'Espinoza', 'Bobadilla', '1915-07-25', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('25174986-8', 'Carlos Álvarez', 'Urrutia', 'Rojas', '1927-05-11', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('45925147-2', 'Carlos Alberto', 'Pasmiño', 'Abarca', '1939-11-07', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('53459254-K', 'Digna Trinidad', 'López', 'Berendsen', '1918-01-19', 2)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('52454583-7', 'Diego Roberto', 'Salazar', 'Contreras', '1918-08-14', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('59537377-8', 'Adam William', 'Baynes', 'Robinson', '1929-02-15', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('13413638-3', 'Alanis Valencia', 'Cortez', 'Benavides', '1921-06-28', 2)
-ON CONFLICT DO NOTHING;
 
 /*
  Personas - Responsables
@@ -94,36 +64,6 @@ VALUES ('13988190-7', 'Joselyn Ana', 'Zúñiga', 'Cordovez', '1951-11-30', 2)
 ON CONFLICT DO NOTHING;
 INSERT INTO residence.person
 VALUES ('13371799-4', 'Ana Maria', 'Aguilar', 'Vicencio', '1951-11-30', 2)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('88843241-8', 'Josefina Valentina', 'Pérez', 'Rodriguez', '1996-06-07', 2)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('86841812-5', 'Mini Alberto', 'Campos', 'Hidalgo', '2000-05-11', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('52424191-9', 'Eljo Vencito', 'Rojas', 'Silvestre', '1998-01-01', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('61263339-8', 'Marcelo Ignacio', 'Espinoza', 'Yáñez', '1988-03-15', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('84698936-6', 'Catalina Antonia', 'Urrutia', 'Gonzalez', '1996-06-07', 2)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('75631611-7', 'Roberto Carlos', 'Pasmiño', 'Gutierrez', '1978-12-04', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('19279146-4', 'Berta Adriana', 'López', 'Campillay', '1979-06-10', 2)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('99596314-0', 'José Manuel', 'Salazar', 'Miranda', '1984-09-18', 1)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('63265479-0', 'Carla María', 'Baynes', 'San Martín', '1993-10-06', 2)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.person
-VALUES ('39119457-2', 'Valentin ignacio', 'Cortez', 'Sandoval', '1990-02-16', 1)
 ON CONFLICT DO NOTHING;
 
 /*
@@ -178,36 +118,6 @@ ON CONFLICT DO NOTHING;
 INSERT INTO residence.responsible
 VALUES ('13371799-4', 67271212)
 ON CONFLICT DO NOTHING;
-INSERT INTO residence.responsible
-VALUES ('88843241-8', 41985112)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.responsible
-VALUES ('86841812-5', 64058694)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.responsible
-VALUES ('52424191-9', 11715538)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.responsible
-VALUES ('61263339-8', 48931984)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.responsible
-VALUES ('84698936-6', 70884998)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.responsible
-VALUES ('75631611-7', 81155567)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.responsible
-VALUES ('19279146-4', 29711729)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.responsible
-VALUES ('99596314-0', 31219500)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.responsible
-VALUES ('63265479-0', 24656825)
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.responsible
-VALUES ('39119457-2', 84627474)
-ON CONFLICT DO NOTHING;
 
 /*
  Cuidadores
@@ -260,34 +170,4 @@ VALUES ('8358513-7', TRUE, '2017-03-21', '12682668-0')
 ON CONFLICT DO NOTHING;
 INSERT INTO residence.elder
 VALUES ('5902831-6', TRUE, '2016-09-03', '14207467-2')
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.elder
-VALUES ('93566783-6', TRUE, '2010-01-01', '88843241-8')
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.elder
-VALUES ('33354753-8', TRUE, '2009-02-05', '86841812-5')
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.elder
-VALUES ('86838888-9', TRUE, '2009-05-09', '52424191-9')
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.elder
-VALUES ('93122719-K', TRUE, '2010-03-17', '61263339-8')
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.elder
-VALUES ('25174986-8', TRUE, '2010-03-17', '84698936-6')
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.elder
-VALUES ('45925147-2', FALSE, '2014-07-29', '75631611-7')
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.elder
-VALUES ('53459254-K', FALSE, '2014-07-29', '19279146-4')
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.elder
-VALUES ('52454583-7', TRUE, '2014-08-03', '99596314-0')
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.elder
-VALUES ('59537377-8', TRUE, '2015-12-16', '63265479-0')
-ON CONFLICT DO NOTHING;
-INSERT INTO residence.elder
-VALUES ('13413638-3', TRUE, '2018-10-20', '39119457-2')
 ON CONFLICT DO NOTHING;
