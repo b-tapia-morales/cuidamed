a06676e90087545ec1e4578965939b5b64615ee7
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-04 09:13:52 -0300

    Capitalized strings in disease names

--- src/main/resources/scripts/data_disease.sql
+++ src/main/resources/scripts/data_disease.sql
@@ -1,20 +1,20 @@
 INSERT INTO residence.disease
-VALUES ('artritis',2,TRUE)
+VALUES ('Artritis', 2, TRUE)
 ON CONFLICT DO NOTHING;
 INSERT INTO residence.disease
-VALUES ('alzheimer',3,TRUE)
+VALUES ('Alzheimer', 3, TRUE)
 ON CONFLICT DO NOTHING;
 INSERT INTO residence.disease
-VALUES ('neumonia',4,TRUE)
+VALUES ('Neumonia', 4, TRUE)
 ON CONFLICT DO NOTHING;
 
 
 INSERT INTO residence.elder_suffers_disease
-VALUES ('8768514-4','artritis','2020-05-08')
+VALUES ('8768514-4', 'Artritis', '2020-05-08')
 ON CONFLICT DO NOTHING;
 INSERT INTO residence.elder_suffers_disease
-VALUES ('5595642-1','alzheimer','2018-07-14')
+VALUES ('5595642-1', 'Alzheimer', '2018-07-14')
 ON CONFLICT DO NOTHING;
 INSERT INTO residence.elder_suffers_disease
-VALUES ('10031785-0','neumonia','2019-06-15')
+VALUES ('10031785-0', 'Neumonia', '2019-06-15')
 ON CONFLICT DO NOTHING;
