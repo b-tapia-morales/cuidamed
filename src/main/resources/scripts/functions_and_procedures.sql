7fb83d439e01978a9acb741d2ba295e6c00edc05
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-18 15:28:13 -0300

    Added function to calculate day difference between current and given date

--- src/main/resources/scripts/functions_and_procedures.sql
+++ src/main/resources/scripts/functions_and_procedures.sql
@@ -1,11 +1,11 @@
 CREATE EXTENSION unaccent;
 
-CREATE FUNCTION remove_accents(text) RETURNS text
+CREATE FUNCTION remove_accents(TEXT) RETURNS TEXT
     LANGUAGE sql
     IMMUTABLE STRICT AS
 'SELECT unaccent(lower($1))';
 
-CREATE FUNCTION extract_days(date) returns INT
+CREATE FUNCTION extract_days(DATE) returns INT
     LANGUAGE sql
     IMMUTABLE STRICT AS
-'SELECT extract(DAY FROM age(CURRENT_DATE, $1))'
+'SELECT (CURRENT_DATE - $1)'
