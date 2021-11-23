d4be9cdae3330d6cb0becabed1192552e425727e
Benjamín López <benjamin.lopezl@userena.cl>
2021-11-23 19:03:10 -0300

    database connection singleton

--- src/main/java/com/bairontapia/projects/cuidamed/dbconnection/DBConnection.java
+++ src/main/java/com/bairontapia/projects/cuidamed/dbconnection/DBConnection.java
@@ -2,23 +2,43 @@ package com.bairontapia.projects.cuidamed.dbconnection;
 import java.sql.*;
 import java.util.*;
 
-public class DBConnection {
+public class DBConnection{
 
-  private static Connection connection = null;
+  private static DBConnection instance;
+  private String url="jdbc:postgresql://10.4.3.195:5432/medicamentos?currentSchema=residence";
+  private String login="medicamentos_dev";
+  private String pass="AsJ65j54";
 
-  static {
-    String url = "jdbc:postgresql://10.4.3.195:5432/medicamentos?currentSchema=residence";
-    String user = "medicamentos_dev";
-    String pass = "AsJ65j54";
+  private DBConnection(){
+
     try {
       Class.forName("org.postgresql.Driver");
-      connection = DriverManager.getConnection(url, user, pass);
-    } catch (ClassNotFoundException | SQLException e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
-  public static Connection getConnection() {
-    return connection;
+  public static Connection getConnection() throws SQLException {
+    if (instance == null) {
+      instance = new DBConnection();
+    }
+    try {
+      return DriverManager.getConnection(instance.url, instance.login,instance.pass);
+    } catch (SQLException e) {
+      throw e;
+    }
   }
+
+  public static void close(Connection connection)
+  {
+    try {
+      if (connection != null) {
+        connection.close();
+        connection=null;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
 }
