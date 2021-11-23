33e8ffaf9ff807fd0d59e4c6ab69aa72682c7c81
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-23 17:11:27 -0300

    Started new branch to dissociate from Spring library

--- src/main/java/com/bairontapia/projects/cuidamed/localization/Commune.java
+++ src/main/java/com/bairontapia/projects/cuidamed/localization/Commune.java
@@ -1,55 +1,6 @@
 package com.bairontapia.projects.cuidamed.localization;
 
-import java.util.Objects;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.Immutable;
-
-@Entity
-@Table(schema = "residence", name = "commune")
-@Immutable
-@Getter
-@Setter(AccessLevel.PRIVATE)
-public class Commune {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id", unique = true, updatable = false, nullable = false)
-  private Short id;
-
-  @Column(name = "commune_name", unique = true, updatable = false, nullable = false)
-  private String name;
-
-  @JoinColumn(name = "province_id", updatable = false, nullable = false)
-  @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-  private Province province;
-
-  @Override
-  public boolean equals(final Object object) {
-    if (this == object) {
-      return true;
-    }
-    if (object instanceof Commune commune) {
-      return Objects.equals(id, commune.id);
-    }
-    return false;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(id);
-  }
+public record Commune(Short id, String name, Short provinceId) {
 
   @Override
   public String toString() {
