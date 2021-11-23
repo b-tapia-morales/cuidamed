008bbc06cc181d9eb85de1cc1d4dda7f4fdf3e11
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-23 17:53:55 -0300

    Refactored equals and hashCode methods to only check for id

--- src/main/java/com/bairontapia/projects/cuidamed/localization/Province.java
+++ src/main/java/com/bairontapia/projects/cuidamed/localization/Province.java
@@ -1,6 +1,24 @@
 package com.bairontapia.projects.cuidamed.localization;
 
-public record Province(Short id, String name, Short regionId) {
+import java.util.Objects;
+
+public record Province(String regionName, Short id, String name) {
+
+  @Override
+  public boolean equals(Object object) {
+    if (this == object) {
+      return true;
+    }
+    if (object instanceof final Province province) {
+      return Objects.equals(id, province.id);
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
 
   @Override
   public String toString() {
--- src/main/java/com/bairontapia/projects/cuidamed/localization/Region.java
+++ src/main/java/com/bairontapia/projects/cuidamed/localization/Region.java
@@ -1,6 +1,24 @@
 package com.bairontapia.projects.cuidamed.localization;
 
+import java.util.Objects;
+
 public record Region(Short id, String name, String abbreviation, String capital) {
+
+  @Override
+  public boolean equals(Object object) {
+    if (this == object) {
+      return true;
+    }
+    if (object instanceof final Region region) {
+      return Objects.equals(id, region.id);
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
 
   @Override
   public String toString() {
