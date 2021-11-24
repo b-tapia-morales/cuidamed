7a614fbdd9ca1202de14b28337d068e5600c6975
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-24 00:29:02 -0300

    Added a static factory method for elder

--- src/main/java/com/bairontapia/projects/cuidamed/person/Elder.java
+++ src/main/java/com/bairontapia/projects/cuidamed/person/Elder.java
@@ -1,12 +1,19 @@
 package com.bairontapia.projects.cuidamed.person;
 
 import com.bairontapia.projects.cuidamed.mappings.gender.Gender;
+import java.sql.Date;
 import java.time.LocalDate;
 import java.util.Objects;
 
 public record Elder(String rut, String fullName, LocalDate birthDate, Gender gender,
                     Boolean isActive, LocalDate admissionDate,
                     String responsibleRut) {
+
+  public static Elder createInstance(String rut, String fullName, Date birthDate, short genderCode,
+      boolean isActive, Date admissionDate, String responsibleRut) {
+    return new Elder(rut, fullName, birthDate.toLocalDate(), Gender.getValueFromCode(genderCode),
+        isActive, admissionDate.toLocalDate(), responsibleRut);
+  }
 
   @Override
   public boolean equals(Object object) {
@@ -28,12 +35,12 @@ public record Elder(String rut, String fullName, LocalDate birthDate, Gender gen
   public String toString() {
     return String.format
         ("""
-                Rut:\t\t\t\t\t\t\t\t\t%s
-                Nombre completo:\t\t\t%s
-                Fecha de nacimiento:\t%s
-                Sexo:\t\t\t\t\t\t\t\t\t%s
-                Activo:\t\t\t\t\t\t\t\t\t%s
-                Fecha de admisión:\t\t%s
-                """, rut, fullName, birthDate, gender, isActive ? "Sí" : "No", admissionDate);
+            Rut:\t\t\t\t\t\t\t\t\t%s
+            Nombre completo:\t\t\t%s
+            Fecha de nacimiento:\t%s
+            Sexo:\t\t\t\t\t\t\t\t\t%s
+            Activo:\t\t\t\t\t\t\t\t\t%s
+            Fecha de admisión:\t\t%s
+            """, rut, fullName, birthDate, gender, isActive ? "Sí" : "No", admissionDate);
   }
 }
