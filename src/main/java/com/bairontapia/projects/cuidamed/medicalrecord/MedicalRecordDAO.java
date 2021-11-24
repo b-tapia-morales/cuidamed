49a2ba5a42781ace73a1dda39d4f70482e5795ee
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-24 10:51:35 -0300

    Added Medical Record DAO template

---
+++ src/main/java/com/bairontapia/projects/cuidamed/medicalrecord/MedicalRecordDAO.java
@@ -0,0 +1,30 @@
+package com.bairontapia.projects.cuidamed.medicalrecord;
+
+import com.bairontapia.projects.cuidamed.daotemplate.NonCompositeIdDAO;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Optional;
+
+public class MedicalRecordDAO implements NonCompositeIdDAO<MedicalRecord, String> {
+
+  @Override
+  public Optional<MedicalRecord> get(String s) throws IOException, SQLException {
+    return Optional.empty();
+  }
+
+  @Override
+  public Collection<MedicalRecord> getAll() throws IOException, SQLException {
+    return null;
+  }
+
+  @Override
+  public void save(MedicalRecord medicalRecord) throws IOException, SQLException {
+
+  }
+
+  @Override
+  public void update(MedicalRecord medicalRecord) throws IOException, SQLException {
+
+  }
+}
