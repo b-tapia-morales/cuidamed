2754786c27bfb3979596bf93466c579d8ae1424c
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-25 12:46:48 -0300

    Created bare bones JavaFX running class

---
+++ src/main/java/com/bairontapia/projects/cuidamed/JavaFXApplication.java
@@ -0,0 +1,18 @@
+package com.bairontapia.projects.cuidamed;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXApplication extends Application {
+
+  public static void main(final String... args) {
+    launch();
+  }
+
+  public void start(final Stage stage) {
+    stage.setTitle("Cuidamed");
+    stage.setResizable(false);
+    stage.show();
+  }
+
+}
