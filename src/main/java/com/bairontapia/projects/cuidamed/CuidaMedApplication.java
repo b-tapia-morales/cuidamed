42f1c453ef15ed678f79cdd14a24eeedec3052e9
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-25 12:47:34 -0300

    Changed main application to run JavaFX app

--- src/main/java/com/bairontapia/projects/cuidamed/CuidaMedApplication.java
+++ src/main/java/com/bairontapia/projects/cuidamed/CuidaMedApplication.java
@@ -1,12 +1,15 @@
 package com.bairontapia.projects.cuidamed;
 
+import com.bairontapia.projects.cuidamed.person.CarerDAO;
 import com.bairontapia.projects.cuidamed.person.ElderDAO;
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class CuidaMedApplication {
 
-  public static void main(String[] args) throws SQLException, IOException {
+  public static void main(String... args) throws SQLException, IOException {
     new ElderDAO().getAll().forEach(System.out::println);
+    new CarerDAO().getAll().forEach(System.out::println);
+    JavaFXApplication.main(args);
   }
 }
