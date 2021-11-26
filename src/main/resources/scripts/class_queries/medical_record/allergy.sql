1db659b947f087346667679a2c1ccfbd03fb4213
Eduardo Palma <eduardoarmando.palma@userena.cl>
2021-11-26 17:30:18 -0300

    update files sql

--- src/main/resources/scripts/class_queries/medical_record/allergy.sql
+++
@@ -1,8 +0,0 @@
-SELECT P.rut,
-       CONCAT(P.first_names, ' ', P.last_name, ' ',
-              P.second_last_name) AS full_name,
-       A.allergy_type,
-       A.allergy_name
-FROM residence.person P,
-     residence.allergy A
-WHERE P.rut = A.rut;
