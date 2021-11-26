08c71180d9872e462e53a4a5ae284e19a48e0bbd
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-26 17:12:45 -0300

    Now using instances instead of object creation

--- src/main/java/com/bairontapia/projects/cuidamed/CuidaMedApplication.java
+++ src/main/java/com/bairontapia/projects/cuidamed/CuidaMedApplication.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 public class CuidaMedApplication {
 
   public static void main(String... args) throws SQLException, IOException {
-    new ElderDAO().getAll().forEach(System.out::println);
-    new CarerDAO().getAll().forEach(System.out::println);
+    ElderDAO.getInstance().getAll().forEach(System.out::println);
+    CarerDAO.getInstance().getAll().forEach(System.out::println);
     JavaFXApplication.main(args);
   }
 }
