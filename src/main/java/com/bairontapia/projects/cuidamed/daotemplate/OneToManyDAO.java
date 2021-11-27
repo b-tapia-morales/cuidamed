c8a1e19563af80a16ed2619f92220cd3187224f8
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-27 01:31:13 -0300

    Created new DAO for One-to-many operations

---
+++ src/main/java/com/bairontapia/projects/cuidamed/daotemplate/OneToManyDAO.java
@@ -0,0 +1,27 @@
+package com.bairontapia.projects.cuidamed.daotemplate;
+
+import com.bairontapia.projects.cuidamed.connection.ConnectionSingleton;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+
+public interface OneToManyDAO<T, ID> extends GenericCrudDAO<T, ID> {
+
+  default Collection<T> get(ID id) throws IOException, SQLException {
+    final var query = findQuery();
+    final var connection = ConnectionSingleton.getInstance();
+    final var statement = connection.prepareStatement(query);
+    setKeyParameter(statement, id);
+    final var resultSet = statement.executeQuery(query);
+    final var set = new LinkedHashSet<T>();
+    while (resultSet.next()) {
+      final T t = readTuple(resultSet);
+      set.add(t);
+    }
+    resultSet.close();
+    statement.close();
+    return set;
+  }
+
+}
