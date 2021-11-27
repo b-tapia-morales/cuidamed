fd16532d95447371d56c6dd83a4668347762074c
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-27 15:10:40 -0300

    Refactored DAOs to modify internal hierarchy

--- src/main/java/com/bairontapia/projects/cuidamed/daotemplate/GenericCrudDAO.java
+++ src/main/java/com/bairontapia/projects/cuidamed/daotemplate/GenericCrudDAO.java
@@ -5,23 +5,11 @@ import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public interface GenericCrudDAO<T, ID> extends GenericReadOnlyDAO<T, ID> {
-
-  String saveQuery() throws IOException;
+public interface GenericCrudDAO<T, ID> extends GenericReadAndWriteDAO<T, ID> {
 
   String updateQuery() throws IOException;
 
-  void saveTuple(PreparedStatement statement, T t) throws SQLException;
-
   void updateTuple(PreparedStatement statement, T t) throws SQLException;
-
-  default void save(T t) throws IOException, SQLException {
-    final var query = saveQuery();
-    final var connection = ConnectionSingleton.getInstance();
-    final var statement = connection.prepareStatement(query);
-    saveTuple(statement, t);
-    statement.close();
-  }
 
   default void update(T t) throws IOException, SQLException {
     var query = updateQuery();
---
+++ src/main/java/com/bairontapia/projects/cuidamed/daotemplate/GenericReadAndWriteDAO.java
@@ -0,0 +1,22 @@
+package com.bairontapia.projects.cuidamed.daotemplate;
+
+import com.bairontapia.projects.cuidamed.connection.ConnectionSingleton;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface GenericReadAndWriteDAO<T, ID> extends GenericReadOnlyDAO<T, ID> {
+
+  String saveQuery() throws IOException;
+
+  void saveTuple(PreparedStatement statement, T t) throws SQLException;
+
+  default void save(T t) throws IOException, SQLException {
+    final var query = saveQuery();
+    final var connection = ConnectionSingleton.getInstance();
+    final var statement = connection.prepareStatement(query);
+    saveTuple(statement, t);
+    statement.close();
+  }
+
+}
