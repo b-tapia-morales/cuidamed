504f25ea2be777659ee2fd7c0e20ee90bafe2ff6
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-26 22:45:00 -0300

    Added responsible fields for elder panel controller

--- src/main/java/com/bairontapia/projects/cuidamed/mvc/person/ElderView.java
+++ src/main/java/com/bairontapia/projects/cuidamed/mvc/person/ElderView.java
@@ -3,8 +3,11 @@ package com.bairontapia.projects.cuidamed.mvc.person;
 import com.bairontapia.projects.cuidamed.mappings.bloodtype.BloodType;
 import com.bairontapia.projects.cuidamed.mappings.gender.Gender;
 import com.bairontapia.projects.cuidamed.mappings.healthcaresystem.HealthCare;
-import com.bairontapia.projects.cuidamed.medicalrecord.MedicalRecordDAO;
+import com.bairontapia.projects.cuidamed.person.Elder;
 import com.bairontapia.projects.cuidamed.person.ElderDAO;
+import com.bairontapia.projects.cuidamed.person.Responsible;
+import com.bairontapia.projects.cuidamed.person.ResponsibleDAO;
+import com.bairontapia.projects.cuidamed.utils.validation.RutUtils;
 import java.io.IOException;
 import java.sql.SQLException;
 import javafx.collections.FXCollections;
@@ -14,6 +17,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
 
 @Getter
 public class ElderView {
@@ -29,6 +33,8 @@ public class ElderView {
   @FXML
   private DatePicker birthDatePicker;
   @FXML
+  private TextField ageField;
+  @FXML
   private ComboBox<Gender> genderComboBox;
   @FXML
   private CheckBox isActiveCheckBox;
@@ -38,23 +44,52 @@ public class ElderView {
   private ComboBox<BloodType> bloodTypeComboBox;
   @FXML
   private ComboBox<HealthCare> healthCareComboBox;
+  @FXML
+  private TextField responsibleRutField;
+  @FXML
+  private TextField responsibleFullNameField;
+  @FXML
+  private TextField responsibleBirthDateField;
+  @FXML
+  private TextField responsibleAgeField;
+  @FXML
+  private TextField responsibleSexField;
+  @FXML
+  private TextField responsibleMobilePhoneField;
 
   public void initialize() throws SQLException, IOException {
     genderComboBox.setItems(FXCollections.observableArrayList(Gender.getValues()));
     bloodTypeComboBox.setItems(FXCollections.observableArrayList(BloodType.getValues()));
     healthCareComboBox.setItems(FXCollections.observableArrayList(HealthCare.getValues()));
     final var elder = ElderDAO.getInstance().get("5875397-1").get();
-    genderComboBox.getSelectionModel().select(elder.gender().ordinal());
-    rutField.setText(elder.rut());
+    fillElderFields(elder);
+    final var responsibleRut = elder.responsibleRut();
+    final var responsible = ResponsibleDAO.getInstance().get(responsibleRut).get();
+    fillResponsibleFields(responsible);
+  }
+
+  private void fillElderFields(final Elder elder) {
+    rutField.setText(RutUtils.format(elder.rut()));
     nameField.setText(elder.firstName());
     lastNameField.setText(elder.lastName());
     secondLastNameField.setText(elder.secondLastName());
     birthDatePicker.setValue(elder.birthDate());
-    if (elder.isActive()) isActiveCheckBox.setSelected(true);
+    ageField.setText(elder.age().toString());
+    genderComboBox.getSelectionModel().select(elder.gender().ordinal());
+    isActiveCheckBox.setSelected(elder.isActive());
     admissionDatePicker.setValue(elder.admissionDate());
   }
 
-
+  private void fillResponsibleFields(final Responsible responsible) {
+    responsibleRutField.setText(RutUtils.format(responsible.rut()));
+    responsibleFullNameField.setText(StringUtils
+        .joinWith(" ", responsible.firstName(), responsible.lastName(),
+            responsible.secondLastName()));
+    responsibleBirthDateField.setText(responsible.birthDate().toString());
+    responsibleAgeField.setText(responsible.age().toString());
+    responsibleSexField.setText(responsible.gender().toString());
+    responsibleMobilePhoneField.setText("+56 9 " + responsible.mobilePhone());
+  }
 
 
 }
