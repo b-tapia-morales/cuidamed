cad34925cf5aa4798062480336fac6877c734611
Benjamín López <benjamin.lopezl@userena.cl>
2021-11-27 14:09:46 -0300

    Updated BloodTypeStatsDAO, SurgicalInterventionDAO and MedicalRecordDAO.

--- src/main/java/com/bairontapia/projects/cuidamed/medicalrecord/MedicalRecordDAO.java
+++ src/main/java/com/bairontapia/projects/cuidamed/medicalrecord/MedicalRecordDAO.java
@@ -1,53 +1,51 @@
 package com.bairontapia.projects.cuidamed.medicalrecord;
 
 import com.bairontapia.projects.cuidamed.connection.ConnectionSingleton;
+import com.bairontapia.projects.cuidamed.daotemplate.GenericReadOnlyDAO;
 import com.bairontapia.projects.cuidamed.daotemplate.ReadOnlyDAO;
 import com.bairontapia.projects.cuidamed.utils.files.TextFileUtils;
 import com.bairontapia.projects.cuidamed.utils.paths.DirectoryPathUtils;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Optional;
 
-public class MedicalRecordDAO implements ReadOnlyDAO<MedicalRecord, String> {
+public class MedicalRecordDAO implements GenericReadOnlyDAO<MedicalRecord, String> {
 
-  private static final String RELATIVE_PATH_STRING = DirectoryPathUtils
-      .relativePathString("scripts", "class_queries", "medical_record");
+  private static MedicalRecordDAO INSTANCE = new MedicalRecordDAO();
+  private static final String RELATIVE_PATH_STRING =
+      DirectoryPathUtils.relativePathString("scripts", "class_queries", "medical_record");
   private static final Path GET_ALL_QUERY_PATH = Path.of(RELATIVE_PATH_STRING, "get_all.sql");
   private static final Path GET_QUERY_PATH = Path.of(RELATIVE_PATH_STRING, "get.sql");
 
-  @Override
-  public Optional<MedicalRecord> get(String rut) throws IOException, SQLException {
-    final var query = TextFileUtils.readString(GET_QUERY_PATH);
-    final var connection = ConnectionSingleton.getInstance();
-    final var statement = connection.prepareStatement(query);
-    statement.setString(1, rut);
-    final var resultSet = statement.executeQuery();
-    if (resultSet.next()) {
-      final var elderRut = resultSet.getString(1);
-      final var bloodTypeCode = resultSet.getShort(2);
-      final var healthCareCode = resultSet.getShort(3);
-      return Optional.of(MedicalRecord.createInstance(elderRut, bloodTypeCode, healthCareCode));
-    }
-    return Optional.empty();
+  public static MedicalRecordDAO getInstance() {
+    return INSTANCE;
   }
 
   @Override
-  public Collection<MedicalRecord> getAll() throws IOException, SQLException {
-    final var query = TextFileUtils.readString(GET_ALL_QUERY_PATH);
-    final var set = new LinkedHashSet<MedicalRecord>();
-    final var connection = ConnectionSingleton.getInstance();
-    final var statement = connection.createStatement();
-    final var resultSet = statement.executeQuery(query);
-    while (resultSet.next()) {
-      final var rut = resultSet.getString(1);
-      final var bloodTypeCode = resultSet.getShort(2);
-      final var healthCareCode = resultSet.getShort(3);
-      final var medicalRecord = MedicalRecord.createInstance(rut, bloodTypeCode, healthCareCode);
-      set.add(medicalRecord);
-    }
-    return set;
+  public String getQuery() throws IOException {
+    return TextFileUtils.readString(GET_QUERY_PATH);
+  }
+
+  @Override
+  public String getAllQuery() throws IOException {
+    return TextFileUtils.readString(GET_ALL_QUERY_PATH);
+  }
+
+  @Override
+  public MedicalRecord readTuple(ResultSet resultSet) throws SQLException {
+    final var elderRut = resultSet.getString(1);
+    final var bloodTypeCode = resultSet.getShort(2);
+    final var healthCareCode = resultSet.getShort(3);
+    return MedicalRecord.createInstance(elderRut, bloodTypeCode, healthCareCode);
+  }
+
+  @Override
+  public void setKeyParameter(PreparedStatement statement, String rut) throws SQLException {
+    statement.setString(1, rut);
   }
 }
