74651918f4824152c61c1cad12a8feaa230045aa
Benjamín López <benjamin.lopezl@userena.cl>
2021-11-27 15:58:08 -0300

    AllergyDAO, BloodTypeStatsDAO, MedicalRecordDAO, and SurgicalInterventionDAO.

--- src/main/java/com/bairontapia/projects/cuidamed/medicalrecord/surgicalintervention/SurgicalInterventionDAO.java
+++ src/main/java/com/bairontapia/projects/cuidamed/medicalrecord/surgicalintervention/SurgicalInterventionDAO.java
@@ -1,81 +1,83 @@
 package com.bairontapia.projects.cuidamed.medicalrecord.surgicalintervention;
 
 import com.bairontapia.projects.cuidamed.connection.ConnectionSingleton;
+import com.bairontapia.projects.cuidamed.daotemplate.GenericReadAndWriteDAO;
 import com.bairontapia.projects.cuidamed.daotemplate.ReadOnlyDAO;
 import com.bairontapia.projects.cuidamed.utils.files.TextFileUtils;
 import com.bairontapia.projects.cuidamed.utils.paths.DirectoryPathUtils;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Optional;
 
-public class SurgicalInterventionDAO implements ReadOnlyDAO<SurgicalIntervention, String> {
+public class SurgicalInterventionDAO
+    implements GenericReadAndWriteDAO<SurgicalIntervention, String> {
 
-  private static final String RELATIVE_PATH_STRING = DirectoryPathUtils
-      .relativePathString("scripts", "class_queries", "surgical_intervention");
+  private static SurgicalInterventionDAO INSTANCE = new SurgicalInterventionDAO();
+  private static final String RELATIVE_PATH_STRING =
+      DirectoryPathUtils.relativePathString("scripts", "class_queries", "surgical_intervention");
   private static final Path FIND_ALL_QUERY_PATH = Path.of(RELATIVE_PATH_STRING, "get_all.sql");
   private static final Path FIND_QUERY_PATH = Path.of(RELATIVE_PATH_STRING, "get.sql");
-  private static final Path UPDATE_QUERY_PATH = Path.of(RELATIVE_PATH_STRING, "update.sql");
-  private static SurgicalInterventionDAO instance;
-
-  private SurgicalInterventionDAO() {
-  }
+  private static final Path SAVE_QUERY_PATH = Path.of(RELATIVE_PATH_STRING, "save.sql");
 
   public static SurgicalInterventionDAO getInstance() {
-    if (instance == null) {
-      instance = new SurgicalInterventionDAO();
-    }
-    return instance;
+    return INSTANCE;
   }
 
   @Override
-  public Optional<SurgicalIntervention> get(String rut) throws IOException, SQLException {
-    final var query = TextFileUtils.readString(FIND_QUERY_PATH);
-    final var connection = ConnectionSingleton.getInstance();
-    final var statement = connection.prepareStatement(query);
+  public String saveQuery() throws IOException {
+    return TextFileUtils.readString(SAVE_QUERY_PATH);
+  }
+
+  @Override
+  public void saveTuple(PreparedStatement statement, SurgicalIntervention surgicalIntervention)
+      throws SQLException {
+    statement.setString(1, surgicalIntervention.rut());
+    statement.setDate(2, Date.valueOf(surgicalIntervention.interventionDate()));
+    statement.setString(3, surgicalIntervention.hospital());
+    statement.setShort(4, surgicalIntervention.severity());
+    statement.setString(5, surgicalIntervention.description());
+    statement.executeUpdate();
+  }
+
+  @Override
+  public String findQuery() throws IOException {
+    return TextFileUtils.readString(FIND_QUERY_PATH);
+  }
+
+  @Override
+  public String findAllQuery() throws IOException {
+    return TextFileUtils.readString(FIND_ALL_QUERY_PATH);
+  }
+
+  @Override
+  public SurgicalIntervention readTuple(ResultSet resultSet) throws SQLException {
+    final var elderRut = resultSet.getString(1);
+    final var firstNames = resultSet.getString(2);
+    final var lastName = resultSet.getString(3);
+    final var secondLastName = resultSet.getString(4);
+    final var interventionDate = resultSet.getDate(5);
+    final var hospital = resultSet.getString(6);
+    final var severity = resultSet.getShort(7);
+    final var description = resultSet.getString(8);
+    return SurgicalIntervention.createInstance(
+        elderRut,
+        firstNames,
+        lastName,
+        secondLastName,
+        interventionDate,
+        hospital,
+        severity,
+        description);
+  }
+
+  @Override
+  public void setKeyParameter(PreparedStatement statement, String rut) throws SQLException {
     statement.setString(1, rut);
-    final var resultSet = statement.executeQuery();
-    if (resultSet.next()) {
-      final var elderRut = resultSet.getString(1);
-      final var firstNames = resultSet.getString(2);
-      final var lastName = resultSet.getString(3);
-      final var secondLastName = resultSet.getString(4);
-      final var interventionDate = resultSet.getDate(5);
-      final var hospital = resultSet.getString(6);
-      final var severity = resultSet.getShort(7);
-      final var description = resultSet.getString(8);
-      final var surgicalIntervention = SurgicalIntervention
-          .createInstance(elderRut, firstNames, lastName, secondLastName, interventionDate,
-              hospital,
-              severity, description);
-      return Optional.of(surgicalIntervention);
-    }
-    return Optional.empty();
-  }
-
-  @Override
-  public Collection<SurgicalIntervention> getAll() throws IOException, SQLException {
-    final var query = TextFileUtils.readString(FIND_ALL_QUERY_PATH);
-    final var connection = ConnectionSingleton.getInstance();
-    final var statement = connection.createStatement();
-    final var resultSet = statement.executeQuery(query);
-    final var set = new LinkedHashSet<SurgicalIntervention>();
-    while (resultSet.next()) {
-      final var rut = resultSet.getString(1);
-      final var firstNames = resultSet.getString(2);
-      final var lastName = resultSet.getString(3);
-      final var secondLastName = resultSet.getString(4);
-      final var interventionDate = resultSet.getDate(5);
-      final var hospital = resultSet.getString(6);
-      final var severity = resultSet.getShort(7);
-      final var description = resultSet.getString(8);
-      final var surgicalIntervention = SurgicalIntervention
-          .createInstance(rut, firstNames, lastName, secondLastName, interventionDate, hospital,
-              severity, description);
-      set.add(surgicalIntervention);
-    }
-    return set;
   }
 }
