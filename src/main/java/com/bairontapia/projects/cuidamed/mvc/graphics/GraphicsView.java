ef58f6085a0b1c7cf5e9fff83052e6587225c729
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-28 16:10:40 -0300

    Refactored some methods

--- src/main/java/com/bairontapia/projects/cuidamed/mvc/graphics/GraphicsView.java
+++ src/main/java/com/bairontapia/projects/cuidamed/mvc/graphics/GraphicsView.java
@@ -1,13 +1,9 @@
 package com.bairontapia.projects.cuidamed.mvc.graphics;
 
 
-import com.bairontapia.projects.cuidamed.medicalrecord.stats.BloodTypeStats;
 import com.bairontapia.projects.cuidamed.medicalrecord.stats.BloodTypeStatsDAO;
-import com.bairontapia.projects.cuidamed.medicalrecord.stats.DiseaseStats;
 import com.bairontapia.projects.cuidamed.medicalrecord.stats.DiseaseStatsDAO;
-import com.bairontapia.projects.cuidamed.medicalrecord.stats.HealthSystemStats;
 import com.bairontapia.projects.cuidamed.medicalrecord.stats.HealthSystemStatsDAO;
-import com.bairontapia.projects.cuidamed.medicalrecord.stats.MedicationStats;
 import com.bairontapia.projects.cuidamed.medicalrecord.stats.MedicationStatsDAO;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -22,19 +18,19 @@ import lombok.Getter;
 public class GraphicsView {
 
   @FXML
-  private ComboBox<TypeGraphic> typeGraphicsCombo;
+  private ComboBox<GraphicSelection> typeGraphicsCombo;
   @FXML
-  private PieChart bloodType;
+  private PieChart bloodTypes;
   @FXML
-  private PieChart healthySystem;
+  private PieChart healthSystems;
   @FXML
-  private PieChart disease;
+  private PieChart diseases;
   @FXML
-  private PieChart medication;
+  private PieChart medications;
 
 
   public void initialize() throws SQLException, IOException {
-    typeGraphicsCombo.setItems(FXCollections.observableArrayList(TypeGraphic.values()));
+    typeGraphicsCombo.setItems(FXCollections.observableArrayList(GraphicSelection.values()));
     loadDataBloodType();
     loadDataHealthySystem();
     loadDataDisease();
@@ -42,30 +38,30 @@ public class GraphicsView {
   }
 
   @FXML
-  public void onGraphicChoice(javafx.event.ActionEvent actionEvent) {
+  public void onGraphicChoice() {
     if (typeGraphicsCombo.getSelectionModel().isSelected(0)) {
-      bloodType.setVisible(true);
-      healthySystem.setVisible(false);
-      disease.setVisible(false);
-      medication.setVisible(false);
+      bloodTypes.setVisible(true);
+      healthSystems.setVisible(false);
+      diseases.setVisible(false);
+      medications.setVisible(false);
     } else {
       if (typeGraphicsCombo.getSelectionModel().isSelected(1)) {
-        healthySystem.setVisible(true);
-        bloodType.setVisible(false);
-        disease.setVisible(false);
-        medication.setVisible(false);
+        healthSystems.setVisible(true);
+        bloodTypes.setVisible(false);
+        diseases.setVisible(false);
+        medications.setVisible(false);
       } else {
         if (typeGraphicsCombo.getSelectionModel().isSelected(2)) {
-          disease.setVisible(true);
-          bloodType.setVisible(false);
-          healthySystem.setVisible(false);
-          medication.setVisible(false);
+          diseases.setVisible(true);
+          bloodTypes.setVisible(false);
+          healthSystems.setVisible(false);
+          medications.setVisible(false);
         } else {
           if (typeGraphicsCombo.getSelectionModel().isSelected(3)) {
-            medication.setVisible(true);
-            disease.setVisible(false);
-            healthySystem.setVisible(false);
-            bloodType.setVisible(false);
+            medications.setVisible(true);
+            diseases.setVisible(false);
+            healthSystems.setVisible(false);
+            bloodTypes.setVisible(false);
           }
         }
       }
@@ -75,50 +71,43 @@ public class GraphicsView {
 
   private void loadDataBloodType() throws SQLException, IOException {
     final var data = BloodTypeStatsDAO.getInstance().findAll();
-    for (BloodTypeStats blood : data) {
-      bloodType.getData().add(new Data(blood.bloodType().getName(), blood.frequency()));
-    }
-    bloodType.setVisible(false);
-    bloodType.setTitle("Tipos de Sangre");
+    data.forEach(e -> bloodTypes.getData().add(new Data(e.bloodType().getName(), e.frequency())));
+    bloodTypes.setVisible(false);
+    bloodTypes.setTitle("Tipos de Sangre");
   }
 
   private void loadDataHealthySystem() throws SQLException, IOException {
     final var data = HealthSystemStatsDAO.getInstance().findAll();
-    for (HealthSystemStats healthy : data) {
-      healthySystem.getData().add(new Data(healthy.healthCare().getName(), healthy.frequency()));
-    }
-    healthySystem.setVisible(false);
-    healthySystem.setTitle("Sistemas de Salud");
+    data.forEach(
+        e -> healthSystems.getData().add(new Data(e.healthCare().getName(), e.frequency())));
+    healthSystems.setVisible(false);
+    healthSystems.setTitle("Sistemas de Salud");
   }
 
   private void loadDataDisease() throws SQLException, IOException {
     final var data = DiseaseStatsDAO.getInstance().findAll();
-    for (DiseaseStats diseaseStats : data) {
-      disease.getData().add(new Data(diseaseStats.diseaseName(), diseaseStats.frequency()));
-    }
-    disease.setVisible(false);
-    disease.setTitle("Enfermedades");
+    data.forEach(e -> diseases.getData().add(new Data(e.diseaseName(), e.frequency())));
+    diseases.setVisible(false);
+    diseases.setTitle("Enfermedades");
   }
 
   private void loadDataMedication() throws SQLException, IOException {
     final var data = MedicationStatsDAO.getInstance().findAll();
-    for (MedicationStats medicationStats : data) {
-      medication.getData().add(new Data(medicationStats.name(), medicationStats.frequency()));
-    }
-    medication.setVisible(false);
-    medication.setTitle("Medicamentos");
+    data.forEach(e -> medications.getData().add(new Data(e.name(), e.frequency())));
+    medications.setVisible(false);
+    medications.setTitle("Medicamentos");
   }
 
   @Getter
-  enum TypeGraphic {
-    BLOODY_TYPE("Tipo de sangre"),
-    HEALTHY_SYSTEM("Sistema de Salud"),
+  enum GraphicSelection {
+    BLOOD_TYPE("Tipo de sangre"),
+    HEALTH_SYSTEM("Sistema de Salud"),
     DISEASE("Enfermedades"),
     MEDICATION("Medicamentos");
 
     private final String name;
 
-    TypeGraphic(String name) {
+    GraphicSelection(String name) {
       this.name = name;
     }
 
