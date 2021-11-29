b09dd68ff4f9e9f284c1924daecb370b7b31e56c
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-28 21:34:29 -0300

    Added fields for responsible

--- src/main/java/com/bairontapia/projects/cuidamed/mvc/person/ElderInsert.java
+++ src/main/java/com/bairontapia/projects/cuidamed/mvc/person/ElderInsert.java
@@ -10,8 +10,11 @@ import com.bairontapia.projects.cuidamed.mappings.bloodtype.BloodType;
 import com.bairontapia.projects.cuidamed.mappings.gender.Gender;
 import com.bairontapia.projects.cuidamed.mappings.healthcaresystem.HealthCare;
 import com.bairontapia.projects.cuidamed.person.elder.Elder;
+import com.bairontapia.projects.cuidamed.utils.validation.RutUtils;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.Period;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -21,6 +24,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 
 @Getter
 public class ElderInsert {
@@ -49,7 +53,19 @@ public class ElderInsert {
   private ComboBox<HealthCare> healthCareComboBox;
 
   @FXML
-  public ComboBox<Gender> responsibleGender;
+  private TextField responsibleRut;
+  @FXML
+  private TextField responsibleName;
+  @FXML
+  private TextField responsibleLastName;
+  @FXML
+  private TextField responsibleSecondLastName;
+  @FXML
+  private DatePicker responsibleBirthDate;
+  @FXML
+  private ComboBox<Gender> responsibleGender;
+  @FXML
+  private TextField responsibleMobilePhone;
 
   @FXML
   private ComboBox<Region> regionComboBox;
@@ -97,5 +113,76 @@ public class ElderInsert {
     final var id = province.id();
     final var communes = CommuneDAO.getInstance().findAll(id);
     communeComboBox.getItems().addAll(communes);
+  }
+
+  private void trimResponsibleFields() {
+    rut.setText(StringUtils.trim(rut.getText()));
+    name.setText(StringUtils.trim(name.getText()));
+    lastName.setText(StringUtils.trim(lastName.getText()));
+    secondLastName.setText(StringUtils.trim(secondLastName.getText()));
+  }
+
+  private void trimElderFields() {
+    responsibleRut.setText(StringUtils.trim(responsibleRut.getText()));
+    responsibleName.setText(StringUtils.trim(responsibleName.getText()));
+    responsibleLastName.setText(StringUtils.trim(responsibleLastName.getText()));
+    responsibleSecondLastName.setText(StringUtils.trim(responsibleSecondLastName.getText()));
+    responsibleMobilePhone.setText(StringUtils.trim(responsibleMobilePhone.getText()));
+  }
+
+  private boolean areElderFieldsEmpty() {
+    return StringUtils.isBlank(rut.getText()) || StringUtils.isBlank(name.getText()) ||
+        StringUtils.isBlank(lastName.getText()) || StringUtils.isBlank(secondLastName.getText()) ||
+        birthDatePicker.getValue() == null || genderComboBox.getSelectionModel().isEmpty() ||
+        admissionDatePicker.getValue() == null;
+  }
+
+  private boolean areElderFieldsTooShort() {
+    final var rutField = rut.getText();
+    final var nameField = name.getText();
+    final var lastNameField = lastName.getText();
+    final var secondLastNameField = secondLastName.getText();
+    return rutField.length() < 9 || nameField.length() < 4 || lastNameField.length() < 4 ||
+        secondLastNameField.length() < 4;
+  }
+
+  private boolean areElderFieldsIncorrect() {
+    final var rutField = rut.getText();
+    final var birthDateField = birthDatePicker.getValue();
+    final var admissionDateField = admissionDatePicker.getValue();
+    final var now = LocalDate.now();
+    final var age = Period.between(birthDateField, now).getYears();
+    final var days = Period.between(admissionDateField, now).getDays();
+    final var years = Period.between(admissionDateField, now).getYears();
+    return !RutUtils.isValid(rutField) || age < 65 || age > 120 || days < 0 || years > 5;
+  }
+
+  private boolean areResponsibleFieldsEmpty() {
+    return StringUtils.isBlank(responsibleRut.getText()) ||
+        StringUtils.isBlank(responsibleName.getText()) ||
+        StringUtils.isBlank(responsibleLastName.getText()) ||
+        StringUtils.isBlank(responsibleSecondLastName.getText()) ||
+        responsibleBirthDate.getValue() == null || genderComboBox.getSelectionModel().isEmpty() ||
+        StringUtils.isBlank(responsibleMobilePhone.getText());
+  }
+
+  private boolean areResponsibleFieldsTooShort() {
+    final var rutField = responsibleRut.getText();
+    final var nameField = responsibleName.getText();
+    final var lastNameField = responsibleLastName.getText();
+    final var secondLastNameField = responsibleSecondLastName.getText();
+    final var mobilePhoneField = responsibleMobilePhone.getText();
+    return rutField.length() < 9 || nameField.length() < 4 || lastNameField.length() < 4
+        || secondLastNameField.length() < 4 || mobilePhoneField.length() < 8;
+  }
+
+  private boolean areResponsibleFieldsIncorrect() {
+    final var rutField = responsibleRut.getText();
+    final var birthDateField = responsibleBirthDate.getValue();
+    final var mobilePhoneField = responsibleMobilePhone.getText();
+    final var now = LocalDate.now();
+    final var age = Period.between(birthDateField, now).getYears();
+    return !RutUtils.isValid(rutField) || age < 18 || age > 65 ||
+        !StringUtils.isNumeric(mobilePhoneField) || Integer.parseInt(mobilePhoneField) < 20_000_000;
   }
 }
