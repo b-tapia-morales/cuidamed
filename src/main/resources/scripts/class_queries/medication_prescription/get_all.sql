a27c26b99890f460ca201bc9148af739e3f40ae4
bairon-tapia <btapiam@alumnosuls.cl>
2021-11-30 21:00:59 -0300

    Updated method to its correct form

--- src/main/resources/scripts/class_queries/medication_prescription/get_all.sql
+++ src/main/resources/scripts/class_queries/medication_prescription/get_all.sql
@@ -1,11 +1,18 @@
+WITH medication_aggregation AS (SELECT DISTINCT MP.rut,
+                                                MP.disease_name,
+                                                string_agg(medication_name, ', ' ORDER BY medication_name) AS medications,
+                                                MP.prescription_date,
+                                                MP.start_date,
+                                                MP.end_date
+                                FROM residence.medication_prescription MP
+                                GROUP BY MP.rut, MP.disease_name, MP.prescription_date,
+                                         MP.start_date, MP.end_date)
 SELECT P.rut,
        CONCAT(P.first_names, ' ', P.last_name, ' ', P.second_last_name) AS full_name,
-       MP.disease_name,
-       string_agg(medication_name, ', ' ORDER BY medication_name)       AS medications,
-       MP.prescription_date,
-       MP.start_date,
-       MP.end_date
+       M.disease_name,
+       M.medications,
+       M.prescription_date,
+       M.start_date,
+       M.end_date
 FROM residence.person P
-         RIGHT OUTER JOIN residence.medication_prescription MP ON P.rut = MP.rut
-GROUP BY P.rut, MP.disease_name, MP.prescription_date, MP.start_date,
-         MP.end_date;
+         RIGHT OUTER JOIN medication_aggregation M ON P.rut = M.rut;
