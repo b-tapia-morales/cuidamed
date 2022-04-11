73e0849ba414395fcf2d9815b4a40b432860d077
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-04-11 00:55:21 -0400

    Fixed last digit verification function

--- src/main/resources/scripts/randomly_generated_inserts/random_rut.sql
+++ src/main/resources/scripts/randomly_generated_inserts/random_rut.sql
@@ -7,23 +7,35 @@ BEGIN
 END
 $func$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION generate_last_digit(rut TEXT) RETURNS CHAR AS
+CREATE OR REPLACE FUNCTION generate_last_digit(rut TEXT) RETURNS CHARACTER AS
 $func$
 DECLARE
-    n     INTEGER DEFAULT length(rut);
-    digit TEXT;
-    sum   INTEGER DEFAULT 0;
-    count INTEGER DEFAULT 2;
+    reverse     TEXT DEFAULT reverse(rut);
+    n           INTEGER DEFAULT length(rut);
+    digit       INTEGER;
+    sum         INTEGER DEFAULT 0;
+    count       INTEGER DEFAULT 2;
+    subtraction INTEGER;
+    last_digit  CHARACTER;
 BEGIN
-    FOR i IN N..2
+    FOR i IN 1..N
         LOOP
-            digit = substr(rut, i, i - 1)::int;
+            digit = substr(reverse, i, 1)::int;
             sum = sum + digit * count;
             count = count + 1;
-            IF (count == 8) THEN
+            IF (count = 8) THEN
                 count = 2;
             END IF;
         END LOOP;
-    RETURN (11 - sum % 11)::char;
+    subtraction = 11 - mod(sum, 11);
+    CASE (subtraction)
+        WHEN 11 THEN last_digit = '0';
+        WHEN 10 THEN last_digit = 'K';
+        ELSE last_digit = subtraction::char;
+        END CASE;
+    RETURN last_digit;
 END
 $func$ LANGUAGE plpgsql;
+
+SELECT *
+FROM generate_last_digit('4646744');
