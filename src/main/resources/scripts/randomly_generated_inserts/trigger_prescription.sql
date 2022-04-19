8bf72a7e67d6c35b6daea1ac84d163faef7fd9ee
Eduardo <eduardoarmando.palma@userena.cl>
2022-04-19 11:29:30 -0400

    update trigger prescription

--- src/main/resources/scripts/randomly_generated_inserts/trigger_prescription.sql
+++ src/main/resources/scripts/randomly_generated_inserts/trigger_prescription.sql
@@ -7,9 +7,19 @@ CREATE OR REPLACE FUNCTION insert_medication_administration() RETURNS TRIGGER AS
 $$
 DECLARE
     carer_rut TEXT ARRAY DEFAULT array(SELECT rut
-                                       FROM carer);
+                                       FROM residence.carer);
     n         INTEGER DEFAULT cardinality(carer_rut);
+    date_current DATE DEFAULT CURRENT_DATE;
 BEGIN
+    IF new.frequency = 2 THEN
+        FOR i in 1..n
+            LOOP
+                date_current = date_current + interval '9 hr';
+                INSERT INTO residence.medication_administration
+                VALUES (new.rut, new.medication_name, date_current, null, 0, carer_rut[floor(random() * (n-1) + 1)]);
+            END LOOP;
+    END IF;
+    IF new.frequency
     INSERT INTO residence.medication_administration
     VALUES (new.rut, new.medication_name, CURRENT_TIMESTAMP(6)+ '10 hr', null, 0, carer_rut[floor(random() * (n-1) + 1)]);
     return new;
@@ -24,3 +34,13 @@ CREATE TRIGGER update_medication_prescription
 execute procedure insert_medication_administration();
 
 SELECT * from residence.medication_administration;
+SELECT * from residence.medication_prescription;
+SELECT * from residence.medication;
+SELECT * from residence.carer;
+
+DELETE FROM residence.medication_administration;
+DELETE FROM residence.medication_prescription;
+
+DELETE FROM residence.medication;
+
+SELECT current_date;
