5a251c37a57831a480739993d6d598001a9e426c
Eduardo <eduardoarmando.palma@userena.cl>
2022-04-19 23:42:24 -0400

    update trigger prescription 2.0

--- src/main/resources/scripts/randomly_generated_inserts/trigger_prescription.sql
+++ src/main/resources/scripts/randomly_generated_inserts/trigger_prescription.sql
@@ -6,22 +6,24 @@ DROP FUNCTION IF EXISTS insert_medication_administration();
 CREATE OR REPLACE FUNCTION insert_medication_administration() RETURNS TRIGGER AS
 $$
 DECLARE
-    carer_rut TEXT ARRAY DEFAULT array(SELECT rut
-                                       FROM residence.carer);
-    n         INTEGER DEFAULT cardinality(carer_rut);
-    date_current DATE DEFAULT CURRENT_DATE;
+    carer_rut    TEXT ARRAY DEFAULT array(SELECT rut
+                                          FROM residence.carer);
+    n            INTEGER DEFAULT cardinality(carer_rut);
+    date_current TIMESTAMP;
+    TIMES       TIME ARRAY DEFAULT ARRAY['12:00:00','06:00:00','04:00:00','03:00:00', '02:20:00'];
 BEGIN
-    IF new.frequency = 2 THEN
-        FOR i in 1..n
-            LOOP
-                date_current = date_current + interval '9 hr';
+    date_current = CURRENT_TIMESTAMP;
+    FOR i in 1..new.frequency
+        LOOP
+            IF i = 1 THEN
                 INSERT INTO residence.medication_administration
-                VALUES (new.rut, new.medication_name, date_current, null, 0, carer_rut[floor(random() * (n-1) + 1)]);
-            END LOOP;
-    END IF;
-    IF new.frequency
-    INSERT INTO residence.medication_administration
-    VALUES (new.rut, new.medication_name, CURRENT_TIMESTAMP(6)+ '10 hr', null, 0, carer_rut[floor(random() * (n-1) + 1)]);
+                VALUES (new.rut, new.medication_name, date_current, null, 0, carer_rut[floor(random() * (n - 1) + 1)]);
+            ELSE
+                INSERT INTO residence.medication_administration
+                VALUES (new.rut, new.medication_name, date_current, null, 0, carer_rut[floor(random() * (n - 1) + 1)]);
+            END IF;
+            date_current = date_current + TIMES[new.frequency];
+        END LOOP;
     return new;
 END
 $$ LANGUAGE plpgsql;
@@ -33,14 +35,25 @@ CREATE TRIGGER update_medication_prescription
     FOR EACH ROW
 execute procedure insert_medication_administration();
 
-SELECT * from residence.medication_administration;
-SELECT * from residence.medication_prescription;
-SELECT * from residence.medication;
-SELECT * from residence.carer;
+SELECT *
+from residence.medication_administration;
+SELECT *
+from residence.medication_prescription;
+SELECT *
+from residence.medication;
+SELECT *
+from residence.carer;
 
-DELETE FROM residence.medication_administration;
-DELETE FROM residence.medication_prescription;
+DELETE
+FROM residence.medication_administration;
+DELETE
+FROM residence.medication_prescription;
 
-DELETE FROM residence.medication;
+DELETE
+FROM residence.medication;
 
 SELECT current_date;
+
+SELECT CAST ('100' AS INTEGER);
+SELECT CURRENT_TIMESTAMP;
+SELECT CONCAT(9::VARCHAR,' hr');
