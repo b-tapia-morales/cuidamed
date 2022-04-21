a91c983809f96fe0fb6e1833fef5f30e1137b0bc
Eduardo <eduardoarmando.palma@userena.cl>
2022-04-20 22:59:05 -0400

    update trigger medication prescription and trigger prescription v1.3

--- src/main/resources/scripts/randomly_generated_inserts/trigger_insert_prescription.sql
+++ src/main/resources/scripts/randomly_generated_inserts/trigger_insert_prescription.sql
@@ -7,11 +7,18 @@ DROP FUNCTION IF EXISTS insert_medication_prescription();
 CREATE OR REPLACE FUNCTION insert_medication_prescription() RETURNS TRIGGER AS
 $$
 DECLARE
-    medication  TEXT ARRAY DEFAULT array(SELECT medication_name FROM residence.medication);
-    n  INTEGER DEFAULT cardinality(medication);
+    medication        TEXT ARRAY DEFAULT array(SELECT medication_name
+                                               FROM residence.medication);
+    n                 INTEGER DEFAULT cardinality(medication);
+    cant_medication INTEGER;
 BEGIN
-    INSERT INTO residence.medication_prescription VALUES (new.rut,new.disease_name,new.prescription_date);
-
+    cant_medication = floor(random() * 3 + 1);
+    FOR i in 1..cant_medication
+        LOOP
+            INSERT INTO residence.medication_prescription
+            VALUES (new.rut, new.disease_name, new.prescription_date, medication[floor(random() * (n - 1) + 1)],
+                new.prescription_date, null, floor(random() * (5 - 2 + 1) + 2)::int, floor(random() * 5 + 1));
+        END LOOP;
     RETURN new;
 end;
 
@@ -22,3 +29,9 @@ CREATE TRIGGER update_prescription
     ON residence.prescription
     FOR EACH ROW
 EXECUTE PROCEDURE insert_medication_prescription();
+
+DELETE FROM residence.prescription;
+
+SELECT * from residence.prescription;
+SELECT * FROM residence.medication_prescription;
+SELECT * FROM residence.medication_administration;
