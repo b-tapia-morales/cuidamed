6116d49163146e45823fbf290e2c27cbad749a1f
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-04-24 20:24:30 -0400

    Changed name of procedures to be more descriptive of what they actually do.

--- src/main/resources/scripts/randomly_generated_inserts/big_insert.sql
+++ src/main/resources/scripts/randomly_generated_inserts/big_insert.sql
@@ -1,18 +1,21 @@
 SET search_path = "residence";
 
-DROP PROCEDURE IF EXISTS batch_insert(n INTEGER);
+DROP PROCEDURE IF EXISTS batch_insert(INTEGER);
 
+DROP INDEX IF EXISTS bmi_btree;
+DROP INDEX IF EXISTS heart_rate_btree;
 
+-- Se crea el procedimiento que se encarga de insertar generar todos los datos al azar.
 CREATE OR REPLACE PROCEDURE batch_insert(n INTEGER)
     LANGUAGE plpgsql AS
 $func$
 BEGIN
-    TRUNCATE person CASCADE;
     TRUNCATE disease CASCADE;
     TRUNCATE medication CASCADE;
+    TRUNCATE person CASCADE;
+    CALL insert_diseases();
+    CALL insert_medications();
     CALL batch_insert_people(n);
-    CALL insert_random_disease();
-    CALL insert_random_medication();
     CALL insert_random_medical_records();
     CALL insert_random_routine_checkups();
     CALL insert_random_prescription();
@@ -21,54 +24,60 @@ $func$;
 
 CALL batch_insert(100);
 
-DROP INDEX IF EXISTS bmi_btree;
-DROP INDEX IF EXISTS heart_rate_btree;
-
+-- ¿Cuántos registros de administraciones de medicamentos generó el procedimiento?
 SELECT count(*) AS medication_administration_count
 FROM medication_administration;
 
+-- Muéstrelos por consola.
 SELECT *
 FROM person p
          INNER JOIN elder e USING (rut)
          RIGHT OUTER JOIN medication_administration mr ON rut = mr.elder_rut;
 
+-- ¿Cuántos registros de chequeos rutinarios generó el procedimiento?
 SELECT count(*) AS routine_checkup_count
 FROM routine_checkup;
 
+-- Muéstrelos por consola.
 SELECT *
 FROM person p
          INNER JOIN elder e USING (rut)
          INNER JOIN routine_checkup rc USING (rut);
 
-EXPLAIN ANALYZE
-SELECT *
-FROM person p
-         INNER JOIN elder e USING (rut)
-         INNER JOIN routine_checkup rc USING (rut)
-WHERE bmi <= 18.00
-   OR bmi >= 30.00;
-
-CREATE INDEX bmi_btree ON routine_checkup USING btree (bmi);
-
-EXPLAIN ANALYZE
-SELECT *
-FROM person p
-         INNER JOIN elder e USING (rut)
-         INNER JOIN routine_checkup rc USING (rut)
-WHERE bmi <= 18.00
-   OR bmi >= 30.00;
-
+-- Primera consulta, sin joins. El índice no ha sido creado aún.
 EXPLAIN analyze
 SELECT *
 FROM routine_checkup
 WHERE heart_rate > 100
-   or heart_rate < 60;
+   OR heart_rate < 60;
 
---CREATE INDEX sys_press_btree ON routine_checkup USING btree (systolic_pressure);
+-- Se crea el índice en el atributo 'heart_rate'.
 CREATE INDEX heart_rate_btree ON routine_checkup USING btree (heart_rate);
 
+-- Se hace la misma consulta, pero esta vez con el índice creado.
 EXPLAIN analyze
 SELECT *
 FROM routine_checkup
 WHERE heart_rate > 100
-   or heart_rate < 60;
+   OR heart_rate < 60;
+
+-- Segunda consulta, además utilizando joins. El índice no ha sido creado aún.
+EXPLAIN ANALYZE
+SELECT *
+FROM person p
+         INNER JOIN elder e USING (rut)
+         INNER JOIN routine_checkup rc USING (rut)
+WHERE bmi <= 18.00
+   OR bmi >= 30.00;
+
+-- Se crea el índice en el atributo 'bmi'.
+CREATE INDEX bmi_btree ON routine_checkup USING btree (bmi);
+
+-- Se hace la misma consulta, pero esta vez con el índice creado.
+EXPLAIN ANALYZE
+SELECT *
+FROM person p
+         INNER JOIN elder e USING (rut)
+         INNER JOIN routine_checkup rc USING (rut)
+WHERE bmi <= 18.00
+   OR bmi >= 30.00;
--- src/main/resources/scripts/randomly_generated_inserts/random_disease_medication.sql
+++ src/main/resources/scripts/randomly_generated_inserts/random_disease_medication.sql
@@ -1,124 +1,9 @@
 SET search_path = "residence";
 
-DROP PROCEDURE IF EXISTS insert_random_medication();
-DROP PROCEDURE IF EXISTS insert_random_disease();
+DROP PROCEDURE IF EXISTS insert_diseases();
+DROP PROCEDURE IF EXISTS insert_medications();
 
-CREATE OR REPLACE PROCEDURE insert_random_medication()
-    LANGUAGE plpgsql AS
-$$
-DECLARE
-    medication_names TEXT ARRAY DEFAULT ARRAY [
-        'Paracetamol',
-        'Ibuprofeno',
-        'Galantamina',
-        'Azitromicina',
-        'Demerol',
-        'Donepezilo',
-        'Levodopa',
-        'Alteplasa',
-        'Riluzol',
-        'Suplementos de Calcio y Vitamina D',
-        'Tolbutamida',
-        'Acebutolol',
-        'Atenolol',
-        'Nitroglicerina',
-        'Duloxetina',
-        'Pregabalina',
-        'Suplementos Vitamínicos',
-        'Lubiprostona',
-        'Pepto Bismol',
-        'Kaopectate',
-        'Codeína',
-        'Morfina',
-        'Alopurinol',
-        'Cloroquina',
-        'Azatioprina',
-        'Metotrexato',
-        'Penicilamina',
-        'Sulfasalazina',
-        'Clorfenamina',
-        'Dexametasona',
-        'Epinefrina',
-        'Hidrocortisona',
-        'Prednisolona',
-        'Carbon Activado',
-        'Acetilcisteína',
-        'Cloruro de Metiltioninio',
-        'Atropina',
-        'Deferoxamina',
-        'Dimercaprol',
-        'Dl Metionina',
-        'Edetato de Calcio y Sodio',
-        'Gluconato de Calcio',
-        'Naloxona',
-        'Nitrito de Sodio',
-        'Tiosulfato de Sodio',
-        'Ácido Valproico',
-        'Carbamazepina',
-        'Diazepam',
-        'Fenitoina',
-        'Fenobarbital',
-        'Sulfato de Magnesio',
-        'Albendazol',
-        'Levamisol',
-        'Mebendazol',
-        'Niclosamida',
-        'Pirantel',
-        'Prazicuantel',
-        'Ivermectina',
-        'Aldinam',
-        'Triciabendazol',
-        'Amoxicilina',
-        'Ampicilina',
-        'Bencilpenicilina',
-        'Cefazolina',
-        'Cefixima',
-        'Cloxacilina',
-        'Fenoximetilpenicilina',
-        'Ciprofloxacino',
-        'Cloranfenicol',
-        'Doxiciclina',
-        'Eritromicina',
-        'Espectinomicina',
-        'Gentamicina',
-        'Metronidazol',
-        'Nitrofurantoína',
-        'Sulfametoxazol',
-        'Trimetoprima',
-        'Clofazimina',
-        'Dapsona',
-        'Rifampicina',
-        'Estreptomicina',
-        'Etambutol',
-        'Isoniazida',
-        'Pirazinamida',
-        'Anafranil',
-        'Amikacina',
-        'Capreomicina',
-        'Cicloserina',
-        'Etionamida',
-        'Kanamicina',
-        'Ofloxacino',
-        'Clotrimazol',
-        'Fluconazol',
-        'Griseofulvina',
-        'Nistatina',
-        'Anfotericina',
-        'Flucitosina',
-        'Ioduro de Potasio',
-        'Aciclovir'];
-    n                INTEGER DEFAULT cardinality(medication_names);
-BEGIN
-    FOR i in 1..n
-        LOOP
-            INSERT INTO medication
-            VALUES (medication_names[i], floor(random() * 2 + 1)::smallint, floor(random() * 4 + 1)::smallint,
-                    floor(random() * 2 + 1)::smallint);
-        END LOOP;
-END;
-$$;
-
-CREATE OR REPLACE PROCEDURE insert_random_disease()
+CREATE OR REPLACE PROCEDURE insert_diseases()
     LANGUAGE plpgsql AS
 $$
 DECLARE
@@ -234,7 +119,117 @@ BEGIN
 END;
 $$;
 
-
-
-
-
+CREATE OR REPLACE PROCEDURE insert_medications()
+    LANGUAGE plpgsql AS
+$$
+DECLARE
+    medication_names TEXT ARRAY DEFAULT ARRAY [
+        'Paracetamol',
+        'Ibuprofeno',
+        'Galantamina',
+        'Azitromicina',
+        'Demerol',
+        'Donepezilo',
+        'Levodopa',
+        'Alteplasa',
+        'Riluzol',
+        'Suplementos de Calcio y Vitamina D',
+        'Tolbutamida',
+        'Acebutolol',
+        'Atenolol',
+        'Nitroglicerina',
+        'Duloxetina',
+        'Pregabalina',
+        'Suplementos Vitamínicos',
+        'Lubiprostona',
+        'Pepto Bismol',
+        'Kaopectate',
+        'Codeína',
+        'Morfina',
+        'Alopurinol',
+        'Cloroquina',
+        'Azatioprina',
+        'Metotrexato',
+        'Penicilamina',
+        'Sulfasalazina',
+        'Clorfenamina',
+        'Dexametasona',
+        'Epinefrina',
+        'Hidrocortisona',
+        'Prednisolona',
+        'Carbon Activado',
+        'Acetilcisteína',
+        'Cloruro de Metiltioninio',
+        'Atropina',
+        'Deferoxamina',
+        'Dimercaprol',
+        'Dl Metionina',
+        'Edetato de Calcio y Sodio',
+        'Gluconato de Calcio',
+        'Naloxona',
+        'Nitrito de Sodio',
+        'Tiosulfato de Sodio',
+        'Ácido Valproico',
+        'Carbamazepina',
+        'Diazepam',
+        'Fenitoina',
+        'Fenobarbital',
+        'Sulfato de Magnesio',
+        'Albendazol',
+        'Levamisol',
+        'Mebendazol',
+        'Niclosamida',
+        'Pirantel',
+        'Prazicuantel',
+        'Ivermectina',
+        'Aldinam',
+        'Triciabendazol',
+        'Amoxicilina',
+        'Ampicilina',
+        'Bencilpenicilina',
+        'Cefazolina',
+        'Cefixima',
+        'Cloxacilina',
+        'Fenoximetilpenicilina',
+        'Ciprofloxacino',
+        'Cloranfenicol',
+        'Doxiciclina',
+        'Eritromicina',
+        'Espectinomicina',
+        'Gentamicina',
+        'Metronidazol',
+        'Nitrofurantoína',
+        'Sulfametoxazol',
+        'Trimetoprima',
+        'Clofazimina',
+        'Dapsona',
+        'Rifampicina',
+        'Estreptomicina',
+        'Etambutol',
+        'Isoniazida',
+        'Pirazinamida',
+        'Anafranil',
+        'Amikacina',
+        'Capreomicina',
+        'Cicloserina',
+        'Etionamida',
+        'Kanamicina',
+        'Ofloxacino',
+        'Clotrimazol',
+        'Fluconazol',
+        'Griseofulvina',
+        'Nistatina',
+        'Anfotericina',
+        'Flucitosina',
+        'Ioduro de Potasio',
+        'Aciclovir'];
+    n                INTEGER DEFAULT cardinality(medication_names);
+BEGIN
+    FOR i in 1..n
+        LOOP
+            INSERT INTO medication
+            VALUES (medication_names[i], floor(random() * 2 + 1)::smallint, floor(random() * 4 + 1)::smallint,
+                    floor(random() * 2 + 1)::smallint);
+        END LOOP;
+END;
+$$;
