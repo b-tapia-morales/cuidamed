2ff7834528574e8a40666ebda9cbd9f009f80964
Benjamín López <benjamin.lopezl@userena.cl>
2022-05-12 16:59:06 -0400

    Added fields to pojos, but PrescriptionPOJO doesn't work.

--- src/main/java/com/bairontapia/projects/cuidamed/pojo/MedicationPrescriptionPOJO.java
+++ src/main/java/com/bairontapia/projects/cuidamed/pojo/MedicationPrescriptionPOJO.java
@@ -1,5 +1,22 @@
 package com.bairontapia.projects.cuidamed.pojo;
 
-public class MedicationPrescriptionPOJO {
+import com.bairontapia.projects.cuidamed.disease.medicationprescription.MedicationPrescription;
+import java.time.LocalDate;
+import lombok.Getter;
 
+public class MedicationPrescriptionPOJO {
+  @Getter private final MedicationPOJO medication;
+  @Getter private final Integer quantity;
+  @Getter private final LocalDate startDate;
+  @Getter private final LocalDate endDate;
+
+  public MedicationPrescriptionPOJO(
+      final MedicationPrescription medicationPrescription,
+      final MedicationPOJO medicationPOJO,
+      final Integer quantity) {
+    medication = medicationPOJO;
+    this.quantity = quantity;
+    startDate = medicationPrescription.startDate();
+    endDate = medicationPrescription.endDate();
+  }
 }
--- src/main/java/com/bairontapia/projects/cuidamed/pojo/PrescriptionPOJO.java
+++ src/main/java/com/bairontapia/projects/cuidamed/pojo/PrescriptionPOJO.java
@@ -1,10 +1,26 @@
 package com.bairontapia.projects.cuidamed.pojo;
 
+import com.bairontapia.projects.cuidamed.disease.Disease;
+import com.bairontapia.projects.cuidamed.disease.medicationprescription.MedicationPrescription;
+import com.bairontapia.projects.cuidamed.disease.prescription.Prescription;
 import java.time.LocalDate;
+import java.util.List;
+import lombok.Getter;
 
 public class PrescriptionPOJO {
-  private DiseasePOJO diseaseName;
-  private LocalDate prescriptionDate;
-  /* List<MedicationPrescriptionPOJO> medicationPrescriptions; */
+  @Getter private final DiseasePOJO disease;
+  @Getter private final LocalDate prescriptionDate;
+  @Getter private final String description;
+  @Getter private final List<MedicationPrescriptionPOJO> medicationPrescriptions;
 
+  public PrescriptionPOJO(
+      final Prescription prescription,
+      final DiseasePOJO diseasePOJO,
+      final List<MedicationPrescription> medicationPrescriptions) {
+    disease = diseasePOJO;
+    prescriptionDate = prescription.prescriptionDate();
+    description = prescription.description();
+    this.medicationPrescriptions =
+        medicationPrescriptions.stream().map(MedicationPrescriptionPOJO::new).toList();
+  }
 }
