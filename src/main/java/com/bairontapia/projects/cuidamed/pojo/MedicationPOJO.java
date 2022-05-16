fceed32bb15f74e847ec92112c545e05d4eb5abc
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-16 00:08:44 -0400

    Revert "Added id's to strong entities"
    This reverts commit 07273172d6e511d16730b87ee288c8be84719cc1.

--- src/main/java/com/bairontapia/projects/cuidamed/pojo/MedicationPOJO.java
+++ src/main/java/com/bairontapia/projects/cuidamed/pojo/MedicationPOJO.java
@@ -2,12 +2,9 @@ package com.bairontapia.projects.cuidamed.pojo;
 
 import com.bairontapia.projects.cuidamed.disease.medication.Medication;
 import lombok.Getter;
-import org.bson.types.ObjectId;
 
 public class MedicationPOJO {
 
-    @Getter
-    private final ObjectId id;
     @Getter
     private final String name;
     @Getter
@@ -18,11 +15,10 @@ public class MedicationPOJO {
     private final String pharmaceuticalForm;
 
     public MedicationPOJO(final Medication medication) {
-        this.id = new ObjectId();
-        this.name = medication.name();
-        this.administrationRoute = medication.administrationRoute().toString();
-        this.measureUnit = medication.measureUnit().toString();
-        this.pharmaceuticalForm = medication.pharmaceuticalForm().toString();
+        name = medication.name();
+        administrationRoute = medication.administrationRoute().toString();
+        measureUnit = medication.measureUnit().toString();
+        pharmaceuticalForm = medication.pharmaceuticalForm().toString();
     }
 
 }
