62637b89913f166e6b0d4bb4bbdf09b306767b8e
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-17 14:21:00 -0400

    Dunno

--- src/main/java/com/bairontapia/projects/cuidamed/mvc/medication/EldersAndMedicationsView.java
+++ src/main/java/com/bairontapia/projects/cuidamed/mvc/medication/EldersAndMedicationsView.java
@@ -6,7 +6,6 @@ import com.bairontapia.projects.cuidamed.disease.medicationprescription.Medicati
 import com.bairontapia.projects.cuidamed.utils.validation.RutUtils;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -26,8 +25,6 @@ public class EldersAndMedicationsView {
     @FXML
     private TableColumn<MedicationPrescription, String> rutColumn;
     @FXML
-    private TableColumn<MedicationPrescription, String> fullNameColumn;
-    @FXML
     private TableColumn<MedicationPrescription, String> nameDiseaseColumn;
     @FXML
     private TableColumn<MedicationPrescription, String> fullMedicationName;
@@ -35,22 +32,24 @@ public class EldersAndMedicationsView {
     private TableColumn<MedicationPrescription, LocalDate> dateStart;
     @FXML
     private TableColumn<MedicationPrescription, LocalDate> dateEnd;
+    private TableColumn<MedicationPrescription, Short> quantity;
+
 
     public void initialize() throws SQLException, IOException {
         rutColumn
                 .setCellValueFactory(e -> new SimpleStringProperty(RutUtils.format(e.getValue().rut())));
-        fullNameColumn.setCellValueFactory(e -> new SimpleStringProperty(e.getValue().fullName()));
         nameDiseaseColumn
                 .setCellValueFactory(e -> new SimpleStringProperty(e.getValue().diseaseName()));
         fullMedicationName
                 .setCellValueFactory(e -> new SimpleStringProperty(e.getValue().medicationName()));
         dateStart.setCellValueFactory(e -> new SimpleObjectProperty<>(e.getValue().startDate()));
         dateEnd.setCellValueFactory(e -> new SimpleObjectProperty<>(e.getValue().endDate()));
+        quantity.setCellValueFactory(e -> new SimpleObjectProperty<>(e.getValue().quantity()));
         final var set = MedicationPrescriptionDAO.getInstance().findAll();
         medicationPrescriptionTableView.getItems().addAll(set);
     }
 
-    public void addedPreciption(ActionEvent actionEvent) throws IOException {
+    public void addedPrescription() throws IOException {
         FXMLLoader fxml = new FXMLLoader();
         Scene scene;
         Stage stage = new Stage();
@@ -61,7 +60,7 @@ public class EldersAndMedicationsView {
         stage.showAndWait();
     }
 
-    public void addedPrescriptionMedication(ActionEvent actionEvent) throws IOException {
+    public void addedPrescriptionMedication() throws IOException {
         FXMLLoader fxml = new FXMLLoader();
         Scene scene;
         Stage stage = new Stage();
