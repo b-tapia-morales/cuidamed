bfde650d9ac75400bc8093ee4f8301591ccff323
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-17 19:37:45 -0400

    Classes now no longer considered as separate collections

--- src/main/java/com/bairontapia/projects/cuidamed/pojo/DiseasePOJO.java
+++ src/main/java/com/bairontapia/projects/cuidamed/pojo/DiseasePOJO.java
@@ -2,12 +2,9 @@ package com.bairontapia.projects.cuidamed.pojo;
 
 import com.bairontapia.projects.cuidamed.disease.Disease;
 import lombok.Getter;
-import org.bson.types.ObjectId;
 
 public class DiseasePOJO {
 
-  @Getter
-  private final ObjectId id;
   @Getter
   private final String name;
   @Getter
@@ -16,7 +13,6 @@ public class DiseasePOJO {
   private final Boolean isChronic;
 
   public DiseasePOJO(final Disease disease) {
-    this.id = new ObjectId();
     this.name = disease.name();
     this.diseaseType = disease.diseaseType().toString();
     this.isChronic = disease.isChronic();
--- src/main/java/com/bairontapia/projects/cuidamed/pojo/MedicationPOJO.java
+++ src/main/java/com/bairontapia/projects/cuidamed/pojo/MedicationPOJO.java
@@ -2,12 +2,9 @@ package com.bairontapia.projects.cuidamed.pojo;
 
 import com.bairontapia.projects.cuidamed.disease.medication.Medication;
 import lombok.Getter;
-import org.bson.types.ObjectId;
 
 public class MedicationPOJO {
 
-  @Getter
-  private final ObjectId id;
   @Getter
   private final String name;
   @Getter
@@ -18,7 +15,6 @@ public class MedicationPOJO {
   private final String pharmaceuticalForm;
 
   public MedicationPOJO(final Medication medication) {
-    this.id = new ObjectId();
     this.name = medication.name();
     this.administrationRoute = medication.administrationRoute().toString();
     this.measureUnit = medication.measureUnit().toString();
