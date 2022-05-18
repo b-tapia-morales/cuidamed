e382bb6669fdd986ccf40ff35cf3efc45e7c5cb8
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-18 00:58:44 -0400

    Added id references to administrations

--- src/main/java/com/bairontapia/projects/cuidamed/pojo/MedicationPrescriptionPOJO.java
+++ src/main/java/com/bairontapia/projects/cuidamed/pojo/MedicationPrescriptionPOJO.java
@@ -3,13 +3,12 @@ package com.bairontapia.projects.cuidamed.pojo;
 import com.bairontapia.projects.cuidamed.disease.medication.Medication;
 import com.bairontapia.projects.cuidamed.disease.medicationprescription.MedicationPrescription;
 import java.time.LocalDate;
+import java.util.List;
 import lombok.Getter;
 import org.bson.types.ObjectId;
 
 public class MedicationPrescriptionPOJO {
 
-  @Getter
-  private final ObjectId id;
   @Getter
   private final MedicationPOJO medication;
   @Getter
@@ -18,13 +17,15 @@ public class MedicationPrescriptionPOJO {
   private final LocalDate endDate;
   @Getter
   private final Short quantity;
+  @Getter
+  private final List<ObjectId> administrationIds;
 
   public MedicationPrescriptionPOJO(final MedicationPrescription medicationPrescription,
-      final Medication medication) {
-    this.id = new ObjectId();
+      final Medication medication, final List<ObjectId> administrationIds) {
     this.medication = new MedicationPOJO(medication);
     this.startDate = medicationPrescription.startDate();
     this.endDate = medicationPrescription.endDate();
     this.quantity = medicationPrescription.quantity();
+    this.administrationIds = administrationIds;
   }
 }
